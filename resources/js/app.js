23bea1875480adba896feadf09a6a24d05420984
W@ngD@nY@ng <59977368+wangdanyang711@users.noreply.github.com>
2021-03-01 07:39:34 -0300

    install vuetify and material design icon font

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,9 +3,14 @@ import store from '~/store'
 import router from '~/router'
 import i18n from '~/plugins/i18n'
 import App from '~/components/App'
+import Vuetify from 'vuetify'
+
+import '@mdi/font/css/materialdesignicons.css'
 
 import '~/plugins'
 import '~/components'
+
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false
 
@@ -14,5 +19,10 @@ new Vue({
   i18n,
   store,
   router,
-  ...App
+  ...App,
+  vuetify: new Vuetify({
+    icons: {
+      iconfont: 'mdi', // default - only for display purposes
+    },
+  }),
 })
--- resources/js/pages/welcome.vue
+++ resources/js/pages/welcome.vue
@@ -18,7 +18,31 @@
 
     <div class="text-center">
       <div class="title mb-4">
-        {{ title }}
+         <v-card
+    class="mx-auto"
+    max-width="300"
+    tile
+  >
+    <v-list dense>
+      <v-subheader>REPORTS</v-subheader>
+      <v-list-item-group
+        v-model="selectedItem"
+        color="primary"
+      >
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
       </div>
 
       <div class="links">
@@ -39,7 +63,13 @@ export default {
   },
 
   data: () => ({
-    title: window.config.appName
+    title: window.config.appName,
+    selectedItem: 1,
+      items: [
+        { text: 'Real-Time', icon: 'mdi-clock' },
+        { text: 'Audience', icon: 'mdi-account' },
+        { text: 'Conversions', icon: 'mdi-flag' },
+      ],
   }),
 
   computed: mapGetters({
