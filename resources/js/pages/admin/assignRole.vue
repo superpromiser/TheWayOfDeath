c8a07700a46e2299d540af8a0398294abfd19491
W@ngD@nY@ng <59977368+wangdanyang711@users.noreply.github.com>
2021-03-03 14:18:45 -0300

    assign role frontend

--- resources/js/pages/admin/assignRole.vue
+++ resources/js/pages/admin/assignRole.vue
@@ -4,14 +4,14 @@
             <v-col cols="12">
                 <v-data-table
                     :headers="headers"
-                    :items="desserts"
+                    :items="userRoleList"
                     class="elevation-1"
                 >
                     <template v-slot:top>
                         <v-toolbar
                             flat
                         >
-                            <v-toolbar-title>My CRUD</v-toolbar-title>
+                            <v-toolbar-title>角色</v-toolbar-title>
                             <v-divider
                             class="mx-4"
                             inset
@@ -21,109 +21,63 @@
                             <v-dialog
                             v-model="dialog"
                             max-width="500px"
+                            scrollable
                             >
-                            <template v-slot:activator="{ on, attrs }">
-                                <v-btn
-                                color="primary"
-                                dark
-                                class="mb-2"
-                                v-bind="attrs"
-                                v-on="on"
-                                >
-                                New Item
-                                </v-btn>
-                            </template>
-                            <v-card>
-                                <v-card-title>
-                                <span class="headline">{{ formTitle }}</span>
-                                </v-card-title>
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+                                    添加
+                                    </v-btn>
+                                </template>
+                                <v-card>
+                                    <v-card-title>
+                                        <span class="headline">{{ formTitle }}</span>
+                                    </v-card-title>
+                                    <v-card-text style="height:400px;">
+                                        <v-container>
+                                            <v-row>
+                                                <v-col cols="12" >
+                                                    <v-text-field
+                                                    v-model="editedItem.roleName"
+                                                    label="Dessert name"
+                                                    ></v-text-field>
+                                                </v-col>
+                                                <v-treeview
+                                                    selectable
+                                                    :items="categoryItems"
+                                                ></v-treeview>
+                                            </v-row>
+                                        </v-container>
+                                    </v-card-text>
 
-                                <v-card-text>
-                                <v-container>
-                                    <v-row>
-                                    <v-col
-                                        cols="12"
-                                        sm="6"
-                                        md="4"
+                                    <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn
+                                        color="blue darken-1"
+                                        text
+                                        @click="close"
                                     >
-                                        <v-text-field
-                                        v-model="editedItem.name"
-                                        label="Dessert name"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        sm="6"
-                                        md="4"
+                                        Cancel
+                                    </v-btn>
+                                    <v-btn
+                                        color="blue darken-1"
+                                        text
+                                        @click="save"
                                     >
-                                        <v-text-field
-                                        v-model="editedItem.calories"
-                                        label="Calories"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        sm="6"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                        v-model="editedItem.fat"
-                                        label="Fat (g)"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        sm="6"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                        v-model="editedItem.carbs"
-                                        label="Carbs (g)"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        sm="6"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                        v-model="editedItem.protein"
-                                        label="Protein (g)"
-                                        ></v-text-field>
-                                    </v-col>
-                                    </v-row>
-                                </v-container>
-                                </v-card-text>
-
-                                <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn
-                                    color="blue darken-1"
-                                    text
-                                    @click="close"
-                                >
-                                    Cancel
-                                </v-btn>
-                                <v-btn
-                                    color="blue darken-1"
-                                    text
-                                    @click="save"
-                                >
-                                    Save
-                                </v-btn>
-                                </v-card-actions>
-                            </v-card>
+                                        Save
+                                    </v-btn>
+                                    </v-card-actions>
+                                </v-card>
                             </v-dialog>
                             <v-dialog v-model="dialogDelete" max-width="500px">
-                            <v-card>
-                                <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
-                                <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                                <v-spacer></v-spacer>
-                                </v-card-actions>
-                            </v-card>
+                                <v-card>
+                                    <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+                                    <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                                    <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                                    <v-spacer></v-spacer>
+                                    </v-card-actions>
+                                </v-card>
                             </v-dialog>
                         </v-toolbar>
                     </template>
@@ -163,39 +117,67 @@
       dialog: false,
       dialogDelete: false,
       headers: [
-        {
-          text: 'Dessert (100g serving)',
-          align: 'start',
-          sortable: false,
-          value: 'name',
-        },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Actions', value: 'actions', sortable: false },
+        { text: '角色', value: 'roleName',  sortable: false, align: 'start' },
+        { text: '创建于', value: 'created_at', sortable: false },
+        { text: '操作', value: 'actions', sortable: false },
       ],
-      desserts: [],
+      userRoleList: [],
       editedIndex: -1,
       editedItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
+        roleName: '',
       },
       defaultItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
+        roleName: '',
       },
+      categoryItems: [
+        {
+          id: 5,
+          name: 'Documents :',
+          children: [
+            {
+              id: 6,
+              name: 'vuetify :',
+              children: [
+                {
+                  id: 7,
+                  name: 'src :',
+                },
+                {
+                  id: 7,
+                  name: 'src :',
+                },
+                {
+                  id: 7,
+                  name: 'src :',
+                },
+              ],
+            },
+            {
+              id: 10,
+              name: 'material2 :',
+              children: [
+                {
+                  id: 11,
+                  name: 'src :',
+                },
+                {
+                  id: 11,
+                  name: 'src :',
+                },
+                {
+                  id: 11,
+                  name: 'src :',
+                },
+              ],
+            },
+          ],
+        },
+      ],
     }),
 
     computed: {
       formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+        return this.editedIndex === -1 ? '新增角色' : '编辑角色'
       },
     },
 
@@ -214,94 +196,45 @@
 
     methods: {
       initialize () {
-        this.desserts = [
+        this.userRoleList = [
           {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
+            roleName: 'admin',
+            created_at: 159,
           },
           {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
+            roleName: 'manager',
+            created_at: 159,
           },
           {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
+            roleName: '老师',
+            created_at: 159,
           },
           {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
+            roleName: '家长',
+            created_at: 159,
           },
           {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
+            roleName: '学生',
+            created_at: 159,
           },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-          },
+          
         ]
       },
 
       editItem (item) {
-        this.editedIndex = this.desserts.indexOf(item)
+        this.editedIndex = this.userRoleList.indexOf(item)
         this.editedItem = Object.assign({}, item)
         this.dialog = true
       },
 
       deleteItem (item) {
-        this.editedIndex = this.desserts.indexOf(item)
+        this.editedIndex = this.userRoleList.indexOf(item)
         this.editedItem = Object.assign({}, item)
         this.dialogDelete = true
       },
 
       deleteItemConfirm () {
-        this.desserts.splice(this.editedIndex, 1)
+        this.userRoleList.splice(this.editedIndex, 1)
         this.closeDelete()
       },
 
@@ -323,9 +256,9 @@
 
       save () {
         if (this.editedIndex > -1) {
-          Object.assign(this.desserts[this.editedIndex], this.editedItem)
+          Object.assign(this.userRoleList[this.editedIndex], this.editedItem)
         } else {
-          this.desserts.push(this.editedItem)
+          this.userRoleList.push(this.editedItem)
         }
         this.close()
       },
