66ece5ee380ef2d8f530421f6536e5875a25f8d7
W@ngD@nY@ng <59977368+wangdanyang711@users.noreply.github.com>
2021-03-03 17:07:47 -0300

    creaeted school manager vue file

--- resources/js/pages/admin/school.vue
+++ resources/js/pages/admin/school.vue
@@ -42,56 +42,38 @@
                                 <v-card-text>
                                 <v-container>
                                     <v-row>
-                                    <v-col
-                                        cols="12"
-                                        sm="6"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                        v-model="editedItem.name"
-                                        label="Dessert name"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        sm="6"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                        v-model="editedItem.calories"
-                                        label="Calories"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        sm="6"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                        v-model="editedItem.fat"
-                                        label="Fat (g)"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        sm="6"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                        v-model="editedItem.carbs"
-                                        label="Carbs (g)"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        sm="6"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                        v-model="editedItem.protein"
-                                        label="Protein (g)"
-                                        ></v-text-field>
-                                    </v-col>
+                                        <v-col cols="12" sm="6" md="4" >
+                                            <v-text-field
+                                            v-model="editedItem.schoolName"
+                                            label="机构名称"
+                                            ></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="4" >
+                                            <v-text-field
+                                            v-model="editedItem.code"
+                                            label="组织机构代码"
+                                            ></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="4" >
+                                            <v-text-field
+                                            v-model="editedItem.phoneNum"
+                                            label="固定电话"
+                                            ></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="4" >
+                                            <v-text-field
+                                            v-model="editedItem.zipCode"
+                                            label="邮编"
+                                            ></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12" sm="6" md="4" >
+                                            <v-text-field
+                                            v-model="editedItem.head"
+                                            label="学校负责人"
+                                            ></v-text-field>
+                                        </v-col>
+                                        
+                                    
                                     </v-row>
                                 </v-container>
                                 </v-card-text>
