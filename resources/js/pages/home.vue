ea353e29d6b421d536265651ce198a8e3457114b
W@ngD@nY@ng <59977368+wangdanyang711@users.noreply.github.com>
2021-03-09 16:47:54 -0300

    home page

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" md="4" sm="6">
+      <v-col v-for="(schoolItem, i) in schoolData" :key="i" cols="12" md="4" sm="6">
         <v-card>
-          <v-img :src="`${baseUrl}/asset/img/login.jpg`" alt="school" ></v-img>
-          <v-card-title>Cafe Badilico</v-card-title>
+          <v-img :src="`${baseUrl}${schoolItem.imgUrl}`" alt="school" ></v-img>
+          <v-card-title>{{schoolItem.schoolName}}</v-card-title>
           <v-card-text>
             <v-row
               align="center"
@@ -24,318 +24,14 @@
               </div>
             </v-row>
 
-            <div class="my-4 subtitle-1">
-              $ • Italian, Cafe
+            <div class="my-2 subtitle-1 d-flex align-center">
+              <v-icon class="mr-2">mdi-account</v-icon>
+              <span><strong>{{schoolItem.head}}</strong></span>
             </div>
 
-            <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4" sm="6">
-        <v-card>
-          <v-img :src="`${baseUrl}/asset/img/login.jpg`" alt="school" ></v-img>
-          <v-card-title>Cafe Badilico</v-card-title>
-          <v-card-text>
-            <v-row
-              align="center"
-              class="mx-0"
-            >
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ml-4">
-                4.5 (413)
-              </div>
-            </v-row>
-
-            <div class="my-4 subtitle-1">
-              $ • Italian, Cafe
-            </div>
-
-            <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4" sm="6">
-        <v-card>
-          <v-img :src="`${baseUrl}/asset/img/login.jpg`" alt="school" ></v-img>
-          <v-card-title>Cafe Badilico</v-card-title>
-          <v-card-text>
-            <v-row
-              align="center"
-              class="mx-0"
-            >
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ml-4">
-                4.5 (413)
-              </div>
-            </v-row>
-
-            <div class="my-4 subtitle-1">
-              $ • Italian, Cafe
-            </div>
-
-            <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4" sm="6">
-        <v-card>
-          <v-img :src="`${baseUrl}/asset/img/login.jpg`" alt="school" ></v-img>
-          <v-card-title>Cafe Badilico</v-card-title>
-          <v-card-text>
-            <v-row
-              align="center"
-              class="mx-0"
-            >
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ml-4">
-                4.5 (413)
-              </div>
-            </v-row>
-
-            <div class="my-4 subtitle-1">
-              $ • Italian, Cafe
-            </div>
-
-            <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4" sm="6">
-        <v-card>
-          <v-img :src="`${baseUrl}/asset/img/login.jpg`" alt="school" ></v-img>
-          <v-card-title>Cafe Badilico</v-card-title>
-          <v-card-text>
-            <v-row
-              align="center"
-              class="mx-0"
-            >
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ml-4">
-                4.5 (413)
-              </div>
-            </v-row>
-
-            <div class="my-4 subtitle-1">
-              $ • Italian, Cafe
-            </div>
-
-            <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4" sm="6">
-        <v-card>
-          <v-img :src="`${baseUrl}/asset/img/login.jpg`" alt="school" ></v-img>
-          <v-card-title>Cafe Badilico</v-card-title>
-          <v-card-text>
-            <v-row
-              align="center"
-              class="mx-0"
-            >
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ml-4">
-                4.5 (413)
-              </div>
-            </v-row>
-
-            <div class="my-4 subtitle-1">
-              $ • Italian, Cafe
-            </div>
-
-            <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4" sm="6">
-        <v-card>
-          <v-img :src="`${baseUrl}/asset/img/login.jpg`" alt="school" ></v-img>
-          <v-card-title>Cafe Badilico</v-card-title>
-          <v-card-text>
-            <v-row
-              align="center"
-              class="mx-0"
-            >
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ml-4">
-                4.5 (413)
-              </div>
-            </v-row>
-
-            <div class="my-4 subtitle-1">
-              $ • Italian, Cafe
-            </div>
-
-            <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4" sm="6">
-        <v-card>
-          <v-img :src="`${baseUrl}/asset/img/login.jpg`" alt="school" ></v-img>
-          <v-card-title>Cafe Badilico</v-card-title>
-          <v-card-text>
-            <v-row
-              align="center"
-              class="mx-0"
-            >
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ml-4">
-                4.5 (413)
-              </div>
-            </v-row>
-
-            <div class="my-4 subtitle-1">
-              $ • Italian, Cafe
-            </div>
-
-            <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4" sm="6">
-        <v-card>
-          <v-img :src="`${baseUrl}/asset/img/login.jpg`" alt="school" ></v-img>
-          <v-card-title>Cafe Badilico</v-card-title>
-          <v-card-text>
-            <v-row
-              align="center"
-              class="mx-0"
-            >
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ml-4">
-                4.5 (413)
-              </div>
-            </v-row>
-
-            <div class="my-4 subtitle-1">
-              $ • Italian, Cafe
-            </div>
-
-            <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4" sm="6">
-        <v-card>
-          <v-img :src="`${baseUrl}/asset/img/login.jpg`" alt="school" ></v-img>
-          <v-card-title>Cafe Badilico</v-card-title>
-          <v-card-text>
-            <v-row
-              align="center"
-              class="mx-0"
-            >
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ml-4">
-                4.5 (413)
-              </div>
-            </v-row>
-
-            <div class="my-4 subtitle-1">
-              $ • Italian, Cafe
-            </div>
-
-            <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4" sm="6">
-        <v-card>
-          <v-img :src="`${baseUrl}/asset/img/login.jpg`" alt="school" ></v-img>
-          <v-card-title>Cafe Badilico</v-card-title>
-          <v-card-text>
-            <v-row
-              align="center"
-              class="mx-0"
-            >
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ml-4">
-                4.5 (413)
-              </div>
-            </v-row>
-
-            <div class="my-4 subtitle-1">
-              $ • Italian, Cafe
+            <div class="my-2 subtitle-1 d-flex align-center">
+              <v-icon class="mr-2">mdi-phone-hangup</v-icon>
+              <span><strong>{{schoolItem.phoneNum}}</strong></span>
             </div>
 
             <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
@@ -347,7 +43,8 @@
   </v-container>
 </template>
 
-<script>
+<script> 
+import { getSchool } from '~/api/school'
 export default {
   middleware: 'auth',
 
@@ -357,7 +54,22 @@ export default {
 
   data: () => ({
     baseUrl: window.Laravel.base_url,
+    schoolData : [],
+    isLoadingSchoolData : false,
+  }),
 
-  })
+  async created(){
+    this.isLoadingSchoolData = true;
+    getSchool()
+      .then((res) => {
+        this.schoolData = res.data;
+        this.isLoadingSchoolData = false;
+      }).catch((err) => {
+        console.log(err);
+        this.isLoadingSchoolData = false;
+      });
+  },
+
+  
 }
 </script>
