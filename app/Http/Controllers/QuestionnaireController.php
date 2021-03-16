c54c5b8a09b011a73fceb3a54899bd1ff0fbe67d
smilecoding7 <superpromiser@gmail.com>
2021-03-16 17:14:31 +0800

    save questionnaire

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -14,8 +14,40 @@ class QuestionnaireController extends Controller
     }
 
     public function createQuestionnaire(Request $request){
-        $postId = Post::create('contentId',1)->id;
-        
+        $this->validate($request,[
+            'title'=>'required',
+            'description'=>'required',
+            'viewList'=>'required',
+            'deadline'=>'required',
+            'questionnaireFlag'=>'required',
+            'resultFlag'=>'required',
+            'answerFlag'=>'required',
+            'content'=>'required',
+        ]);
+        $title = $request->title;
+        $desc = $request->description;
+        $viewList = json_encode($request->viewList);
+        $deadline = $request->deadline;
+        $questionnaireFlg = $request->questionnaireFlag;
+        $resultFlag = $request->resultFlag;
+        $answerFlag = $request->answerFlag;
+        $content = json_encode($request->content);
+        $postId = Post::create(['contentId'=>1])->id;
+        $data = Questionnaire::create([
+            'title'=>$title,
+            'desc'=>$desc,
+            'viewList'=>$viewList,
+            'deadline'=>$deadline,
+            'questionnaireFlag'=>$questionnaireFlg,
+            'resultFlag'=>$resultFlag,
+            'answerFlag'=>$answerFlag,
+            'content'=>$content,
+            'postId'=>$postId
+        ]);
+        return response()->json([
+            'data'=>$data,
+            'state'=>201
+        ]);
     }
 
     public function updateQuestionnaire(){
--- app/Questionnaire.php
+++ app/Questionnaire.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Questionnaire extends Model
 {
     //
+    protected $guarded = [];
 }
