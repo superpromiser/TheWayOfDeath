bca503be27b480cdfda0cc977e349b0188dc1701
W@ngD@nY@ng <59977368+wangdanyang711@users.noreply.github.com>
2021-03-21 18:59:17 -0300

    questionnaire view

--- resources/js/pages/school/details/questionnaire/index.vue
+++ resources/js/pages/school/details/questionnaire/index.vue
@@ -27,12 +27,118 @@
             </template>
         </v-banner>
         <!----title---->
-        <v-row>
+        <v-row class="ma-0 px-5 px-md-10">
             <v-col cols="12" class="d-flex justify-center align-center">
                 <h2>{{contentData.questionnaires.title}}</h2>
             </v-col>
-            <v-col cols="12" class="px-5 px-md-10">
+            <v-col cols="12">
                 <p>{{contentData.questionnaires.desc}}</p>
+            </v-col>
+        </v-row>
+        <!----questionnaires---->
+        <v-row class="ma-0 px-5 px-md-10">
+            <v-col cols="12" v-for="(content, index) in contentData.questionnaires.content" :key="index">
+                <!--  single Datas  -->
+                <v-row v-if="content.type == 'single'">
+                  <v-col cols="12">
+                    <p class="mb-0 d-flex align-center"> 
+                      {{index + 1}}.  
+                      <v-chip class="ma-2" color="success" outlined >
+                        <strong>单选题</strong>
+                      </v-chip>
+                    </p>
+                    <p class="text-wrap pl-3 mb-0">{{ content.singleContentDataArr[0].text }}</p>
+                  </v-col>
+                  <v-col v-if="checkIfAttachExist(content.singleContentDataArr[0])">
+                    <AttachItemViewer :items="content.singleContentDataArr[0]" />
+                  </v-col>
+                  <v-col class="pl-6" cols="12" v-for="(singleData, singleDataIndex) in content.singleContentDataArr" :key="singleDataIndex" v-if="singleDataIndex !== 0">
+                    <div class="d-flex align-center"> 
+                      <v-chip
+                        class="mr-2"
+                        color="success"
+                        outlined
+                      >
+                        <strong>{{alphabet[singleDataIndex-1]}}</strong>
+                      </v-chip>
+                      <p class="mb-0 text-wrap"> {{singleData.text}}</p>
+                    </div>
+                    <AttachItemViewer :items="singleData" v-if="checkIfAttachExist(singleData)" />
+                  </v-col>
+                </v-row>
+                <!--  multi Datas  -->
+                <v-row v-if="content.type == 'multi'">
+                  <v-col cols="12">
+                    <p class="mb-0 d-flex align-center"> 
+                      {{index + 1}}.  
+                      <v-chip class="ma-2" color="success" outlined >
+                        <strong>多选题</strong>
+                      </v-chip>
+                    </p>
+                    <p class="text-wrap pl-3 mb-0">{{ content.multiContentDataArr[0].text }}</p>
+                  </v-col>
+                  <v-col v-if="checkIfAttachExist(content.multiContentDataArr[0])">
+                    <AttachItemViewer :items="content.multiContentDataArr[0]" />
+                  </v-col>
+                  <v-col class="pl-6" cols="12" v-for="(multiData, singleDataIndex) in content.multiContentDataArr" :key="singleDataIndex" v-if="singleDataIndex !== 0">
+                    <div class="d-flex align-center"> 
+                      <v-chip
+                        class="mr-2"
+                        color="success"
+                        outlined
+                      >
+                        <strong>{{alphabet[singleDataIndex-1]}}</strong>
+                      </v-chip>
+                      <p class="mb-0 text-wrap"> {{multiData.text}}</p>
+                    </div>
+                    <AttachItemViewer :items="multiData" v-if="checkIfAttachExist(multiData)" />
+                  </v-col>
+                </v-row>
+                <!--  qa Datas  -->
+                <v-row v-if="content.type == 'qa'">
+                  <v-col cols="12">
+                    <p class="mb-0 d-flex align-center"> 
+                      {{index + 1}}.  
+                      <v-chip class="ma-2" color="success" outlined >
+                        <strong>问答题</strong>
+                      </v-chip>
+                    </p>
+                    <p class="text-wrap pl-3 mb-0">{{ content.qaContentDataArr[0].text }}</p>
+                  </v-col>
+                  <v-col v-if="checkIfAttachExist(content.qaContentDataArr[0])">
+                    <AttachItemViewer :items="content.qaContentDataArr[0]" />
+                  </v-col>
+                </v-row>
+                <!--  statistics Datas  -->
+                <v-row v-if="content.type == 'stat'">
+                  <v-col cols="12">
+                    <p class="mb-0 d-flex align-center"> 
+                      {{index + 1}}.  
+                      <v-chip class="ma-2" color="success" outlined >
+                        <strong>统计题</strong>
+                      </v-chip>
+                    </p>
+                    <p class="text-wrap pl-3 mb-0">{{ content.statDataArr[0].contentData[0].text }}</p>
+                  </v-col>
+                  <v-col v-if="checkIfAttachExist(content.statDataArr[0].contentData[0])">
+                    <AttachItemViewer :items="content.statDataArr[0].contentData[0]" />
+                  </v-col>
+                </v-row>
+                <!--  score Datas  -->
+                <v-row v-if="content.type == 'score'">
+                  <v-col cols="12">
+                    <p class="mb-0 d-flex align-center"> 
+                      {{index + 1}}.  
+                      <v-chip class="ma-2" color="success" outlined >
+                        <strong>评分题</strong>
+                      </v-chip>
+                    </p>
+                    <p class="text-wrap pl-3 mb-0">{{ content.scoringDataArr[0].contentData[0].text }}</p>
+                  </v-col>
+                  <v-col v-if="checkIfAttachExist(content.scoringDataArr[0].contentData[0])">
+                    <AttachItemViewer :items="content.scoringDataArr[0].contentData[0]" />
+                  </v-col>
+                </v-row>
             </v-col>
         </v-row>
     </v-container>
@@ -40,12 +146,18 @@
 
 <script>
 import {mapGetters} from 'vuex';
-import lang from '~/helper/lang.json'
+import lang from '~/helper/lang.json';
+import AttachItemViewer from '~/components/attachItemViewer';
 export default {
+    components:{
+        AttachItemViewer,
+    },
+
     data:()=>({
         baseUrl:window.Laravel.base_url,
         lang,
-
+        alphabet:['A','B','C','D','E','F','G','H','J','K','L','M','N',
+                'O','P','Q','R','S','T','U','V','W','X','Y','Z' ],
     }),
 
     computed:{
@@ -61,6 +173,19 @@ export default {
         console.log("this.currentpath",this.currentpath)
         console.log("this.contentData",this.contentData)
         this.contentData.questionnaires.content = JSON.parse(this.contentData.questionnaires.content);
+    },
+
+    methods:{
+        checkIfAttachExist(data){
+            let count = 0;
+            count = count + data.imgUrl.length + data.videoUrl.length + data.otherUrl.length;
+            if( count == 0 ) {
+                return false;
+            }
+            else{
+                return true;
+            }
+        },
     }
 }
 </script>
