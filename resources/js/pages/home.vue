c8404e4ddf5ea5b918f4b9f96ebf2c340005f2c3
W@ngD@nY@ng <59977368+wangdanyang711@users.noreply.github.com>
2021-03-24 21:23:17 -0300

    update home page

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -1,29 +1,11 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col v-for="(schoolItem, i) in schoolData" :key="i" cols="12" md="4" sm="6">
+    <v-row v-if="user.roleId == 1">
+      <v-col v-for="(schoolItem, i) in schoolList" :key="i" cols="12" md="4" sm="6" class="hover-cursor-point" @click="goNewsOfSchool(schoolItem.id)">
         <v-card>
           <v-img :src="`${baseUrl}${schoolItem.imgUrl}`" alt="school" class="school-card-img"></v-img>
           <v-card-title>{{schoolItem.schoolName}}</v-card-title>
           <v-card-text>
-            <v-row
-              align="center"
-              class="mx-0"
-            >
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ml-4">
-                4.5 (413)
-              </div>
-            </v-row>
-
             <div class="my-2 subtitle-1 d-flex align-center">
               <v-icon class="mr-2">mdi-account</v-icon>
               <span><strong>{{schoolItem.head}}</strong></span>
@@ -34,16 +16,32 @@
               <span><strong>{{schoolItem.phoneNum}}</strong></span>
             </div>
 
-            <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+            <div>{{schoolItem.introduce}}</div>
           </v-card-text>
         </v-card>
       </v-col>
     </v-row>
-    
+    <v-row v-else-if="user.roleId == 2">
+      <v-col v-for="(lesson, i) in mySchoolLessonData" :key="i" cols="12" md="4" sm="6" class="hover-cursor-point" @click="goNewsOfClass(lesson)">
+        <v-card>
+          <v-img :src="`${baseUrl}${lesson.imgUrl}`" alt="school" class="school-card-img"></v-img>
+          <v-card-title>{{lesson.lessonName}} - {{lesson.gradeName}}</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row v-else>
+      <v-col cols="12" md="4" sm="6" @click="goNewsOfClass(myLessonData)" class="hover-cursor-point">
+        <v-card>
+          <v-img :src="`${baseUrl}${myLessonData.imgUrl}`" alt="school" class="school-card-img"></v-img>
+          <v-card-title>{{myLessonData.lessonName}} - {{myLessonData.gradeName}}</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script> 
+import { mapGetters } from 'vuex'
 import { getSchool } from '~/api/school'
 export default {
   middleware: 'auth',
@@ -54,20 +52,67 @@ export default {
 
   data: () => ({
     baseUrl: window.Laravel.base_url,
-    schoolData : [],
+    schoolList : [],
     isLoadingSchoolData : false,
+    mySchoolLessonData: [],
+    myLessonData:{}
   }),
 
-  async created(){
-    this.isLoadingSchoolData = true;
-    getSchool()
-      .then((res) => {
-        this.schoolData = res.data;
-        this.isLoadingSchoolData = false;
-      }).catch((err) => {
-        //console.log(err);
-        this.isLoadingSchoolData = false;
-      });
+  computed:{
+    ...mapGetters({
+      user : 'auth/user',
+      schoolTree : 'schooltree/schoolData'
+    })
   },
+
+  async created(){
+    if(this.user.roleId == 1){
+      this.isLoadingSchoolData = true;
+      getSchool()
+        .then((res) => {
+          this.schoolList = res.data;
+          this.isLoadingSchoolData = false;
+        }).catch((err) => {
+          console.log(err);
+          this.isLoadingSchoolData = false;
+        });
+    }
+    else if (this.user.roleId == 2){
+      let myschoolData = {}
+      this.schoolTree.map(x=>{
+        if(this.user.schoolId == x.id){
+          myschoolData = x;
+        }
+      })
+      myschoolData.grades.map(grade=>{
+        grade.lessons.map(lesson=>{
+          lesson["gradeName"] = grade.gradeName;
+          this.mySchoolLessonData.push(lesson);
+        })
+      })
+
+    }
+    else{
+      this.schoolTree.map(school=>{
+        school.grades.map(grade=>{
+          grade.lessons.map(lesson=>{
+            if(this.user.lessonId == lesson.id){
+              this.myLessonData = lesson;
+              this.myLessonData["gradeName"] = grade.gradeName;
+            }
+          })
+        })
+      })
+    }
+  },
+
+  methods:{
+    goNewsOfSchool(schoolId){
+      this.$router.push({name: 'schoolSpace', params:{schoolId: schoolId}});
+    },
+    goNewsOfClass(lesson){
+      this.$router.push({name: 'classSpace', params:{schoolId: lesson.schoolId, gradeId: lesson.gradeId, lessonId: lesson.id}});
+    }
+  }
 }
 </script>
