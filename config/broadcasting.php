05d9922daf5807c23eec8af17189d27fb15acc34
W@ngD@nY@ng <59977368+wangdanyang711@users.noreply.github.com>
2021-03-25 11:49:51 -0300

    make foundation of chat

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,11 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
+                // 'useTLS' => true,
+                 'host' => '127.0.0.1',
+                // 'host' => '8.131.231.180',
+                'port' => 6001,
+                'scheme' => 'http'
             ],
         ],
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -14,6 +14,20 @@ import '~/components'
 
 Vue.use(Vuetify);
 
+import Echo from 'laravel-echo';
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    forceTLS: false,
+    disableStats: true,
+});
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
