74129015d309d5ef658c83e7246cb7d8bbf3eaea
smilecoding7 <superpromiser@gmail.com>
2021-04-04 19:20:17 +0800

    questionnaire detail

--- app/Http/Controllers/AnswerQuestionnaireController.php
+++ app/Http/Controllers/AnswerQuestionnaireController.php
@@ -10,37 +10,36 @@ use Illuminate\Support\Facades\Auth;
 class AnswerQuestionnaireController extends Controller
 {
     //
-    public function getAnswerQuestionnaire(Request $request){
-        $this->validate($request,[
-            'postId'=>'required'
+    public function getAnswerQuestionnaire(Request $request)
+    {
+        $this->validate($request, [
+            'postId' => 'required'
         ]);
-        return AnswerQuestionnaire::where('postId',$request->postId)->get();
+        return AnswerQuestionnaire::where('postId', $request->postId)->get();
     }
 
-    public function createAnswerQuestionnaire(Request $request){
-        $this->validate($request,[
+    public function createAnswerQuestionnaire(Request $request)
+    {
+        $this->validate($request, [
             // 'singleAnswer'=>'required',
             // 'multiAnswer'=>'required',
             // 'questionAnswer'=>'required',
             // 'statAnswer'=>'required',
             // 'scoringAnswer'=>'required',
-            'postId'=>'required',
+            'postId' => 'required',
+            'answerData' => 'required'
         ]);
         $userId = Auth::user()->id;
-        $answerId = Answer::create([
-            'postId'=>$request->postId,
-            'userId'=>$userId
-        ])->id;
+        // $answerId = Answer::create([
+        //     'postId'=>$request->postId,
+        //     'userId'=>$userId
+        // ])->id;
         return AnswerQuestionnaire::create([
-            'singleAnswer'=>$request->singleAnswer,
-            'multiAnswer'=>json_encode($request->multiAnswer),
-            'questionAnswer'=>$request->questionAnswer,
-            'statAnswer'=>$request->statAnswer,
-            'scoringAnswer'=>$request->scoringAnswer,
-            'answerId'=>$answerId,
-            'postId'=>$request->postId,
-            'userId'=>$userId
+            'answerData' => json_encode($request->answerData),
+            'schoolId' => $request->schoolId,
+            'lessonId' => $request->lessonId,
+            'postId' => $request->postId,
+            'userId' => $userId
         ]);
-
     }
 }
