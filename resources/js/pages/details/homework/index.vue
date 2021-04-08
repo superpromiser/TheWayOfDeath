ee2a62daa0a3f783a2b5fe042a0e3446e303733b
smilecoding7 <superpromiser@gmail.com>
2021-04-08 18:16:14 +0800

    homework result

--- resources/js/pages/details/homework/index.vue
+++ resources/js/pages/details/homework/index.vue
@@ -1,34 +1,39 @@
 <template>
     <v-container>
-        <v-col cols="12" class="d-flex align-center hover-cursor-point" @click="showDetail">
+        <v-col cols="12" class="d-flex align-cente">
+            <a @click="$router.go(-1)" class="float-left">
+                <v-icon size="70">
+                    mdi-chevron-left
+                </v-icon>
+            </a>
             <v-avatar class="ma-3 school-card-avatar" tile >
-              <v-img :src="`${baseUrl}/asset/img/newIcon/作业.png`" alt="postItem" ></v-img>
+                <v-img :src="`${baseUrl}/asset/img/newIcon/作业.png`" alt="postItem" ></v-img>
             </v-avatar>
             <div>
-              <p class="font-weight-black fs-15 mb-3"> {{lang.homework}}  </p>
-              <div class="d-flex align-center">
+            <p class="font-weight-black fs-15 mb-3"> {{lang.notification}}  </p>
+            <div class="d-flex align-center">
                 <v-icon medium color="primary" class="mr-2">mdi-clock-outline </v-icon>
-                <p class="mb-0 mr-8">{{TimeView(content.created_at)}}</p>
+                <p class="mb-0 mr-8">{{TimeView(contentData.created_at)}}</p>
                 <v-icon medium color="primary" class="mr-2">mdi-account </v-icon>
-                <p class="mb-0">{{content.users.name}}</p>
-              </div>
+                <p class="mb-0">{{contentData.users.name}}</p>
+            </div>
             </div>
             <div class="ml-auto">
-              <v-menu offset-y >
+            <v-menu offset-y >
                 <template v-slot:activator="{ attrs, on }">
-                  <v-btn icon color="primary" v-bind="attrs" v-on="on" >
+                <v-btn icon color="primary" v-bind="attrs" v-on="on" >
                     <v-icon size="30">mdi-chevron-down </v-icon>
-                  </v-btn>
+                </v-btn>
                 </template>
                 <v-list>
-                  <v-list-item link >
+                <v-list-item link >
                     <v-list-item-title class="px-2">{{lang.toTop}}</v-list-item-title>
-                  </v-list-item>
-                  <v-list-item link >
-                    <v-list-item-title class="px-2" @click="postRemove(content)">{{lang.remove}}</v-list-item-title>
-                  </v-list-item>
+                </v-list-item>
+                <v-list-item link >
+                    <v-list-item-title class="px-2" @click="postRemove(contentData)">{{lang.remove}}</v-list-item-title>
+                </v-list-item>
                 </v-list>
-              </v-menu>
+            </v-menu>
             </div>
         </v-col>
         <v-col cols="12" class="pl-10 pt-0">
@@ -58,49 +63,47 @@
             </v-col>
           </v-row>
         </v-col>
+        <div>
+            <router-view></router-view>
+        </div>
     </v-container>
 </template>
 
 <script>
-import AttachItemViewer from '~/components/attachItemViewer';
 import lang from '~/helper/lang.json'
+import {mapGetters} from 'vuex';
+import AttachItemViewer from '~/components/attachItemViewer';
 export default {
     components:{
         AttachItemViewer,
     },
-    props:{
-        content:{
-            type:Object,
-            required:true
-        }
-    },
     data:()=>({
-        lang,
         baseUrl:window.Laravel.base_url,
-        homeworkData: null,
+        lang,
+        homeworkData:null
     }),
     computed:{
-      currentPath(){
-        return this.$route
-      }
+         currentpath(){
+            return this.$route;
+        },
+        ...mapGetters({
+            contentData:'content/postDetail',
+            user:'auth/user'
+        })
     },
     created(){
-        console.log(this.content)
-        if(this.content){
-            this.homeworkData = this.content.homework
-            this.homeworkData.content = JSON.parse(this.homeworkData.content)
+        if(this.contentData == null){
+            if(this.currentpath.params.lessonId){
+                this.$router.push({name:'classSpace.news'})
+            }else{
+                this.$router.push({name:'schoolSpace.news'})
+            }
         }
-    },
-    methods:{
-      showDetail(){
-        this.$store.dispatch('content/storePostDetail',this.content)
-        if(this.currentPath.params.lessonId){
-          this.$router.push({name:'details.classHomework'});
-        }else{
-          this.$router.push({name:'details.homework'});
-        }
-      }
+        console.log(this.contentData)
+        this.homeworkData = this.contentData.homework
+
     }
+
 }
 </script>
 
