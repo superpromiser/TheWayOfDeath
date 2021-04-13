46fc58a486318fb16a5b5b5750882ddf152ca4ad
smilecoding7 <superpromiser@gmail.com>
2021-04-13 16:42:30 +0800

    test 94

--- resources/js/pages/admin/schedule/teacher.vue
+++ resources/js/pages/admin/schedule/teacher.vue
@@ -525,6 +525,8 @@ export default {
                     console.log(i)
                 }else if(i == 50){
                     console.log(i)
+                }else if(i == 50){
+                    console.log(i)
                 }
             }
         }
