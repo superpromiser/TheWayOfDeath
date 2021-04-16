db3cdb0476b71978e37cdc79b0a882699d2525ce
W@ngD@nY@ng <59977368+wangdanyang711@users.noreply.github.com>
2021-04-16 11:18:00 -0300

    schoolsapce home visit mo

--- resources/js/pages/school/posts/schoolStory/index.vue
+++ resources/js/pages/school/posts/schoolStory/index.vue
@@ -1,14 +1,14 @@
 <template>
     <v-container v-if="$isMobile()">
         <v-row class="ma-0">
-            <v-col cols="12" class="mo-glow d-flex align-center">
+            <v-col cols="12" class="mo-glow d-flex align-center justify-center">
                 <v-avatar class="mo-glow-small-shadow" >
-                    <v-img :src="`${baseUrl}/asset/img/newIcon/校园动态.png`" alt="postItem" width="48" height="48" ></v-img>
+                    <v-img :src="`${baseUrl}/asset/img/appIcon/校园文化/校园动态.png`" alt="postItem" width="48" height="48" ></v-img>
                 </v-avatar>
                 <h2 class="ml-3">{{lang.schoolStory}}</h2>
             </v-col>
         </v-row>
-        <v-row class="ma-0 mo-glow mt-5">
+        <v-row class="ma-0 mo-glow">
             <v-col cols="12">
                 <QuestionItem :Label="lang.contentPlaceFirst" :emoji="true" :contact="true"  ref="child" @contentData="loadContentData"></QuestionItem>
             </v-col>
