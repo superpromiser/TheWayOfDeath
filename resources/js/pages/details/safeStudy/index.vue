2e5bbfb06661dceefe0a27eee5318531e64a571b
W@ngD@nY@ng <59977368+wangdanyang711@users.noreply.github.com>
2021-04-21 18:54:20 -0300

    safe study detail web

--- resources/js/pages/details/safeStudy/index.vue
+++ resources/js/pages/details/safeStudy/index.vue
@@ -1,49 +1,22 @@
 <template>
     <v-container>
-        <v-col cols="12" class="d-flex align-center">
-            <a @click="$router.go(-1)" class="float-left">
-                <v-icon size="70">
-                    mdi-chevron-left
-                </v-icon>
-            </a>
-            <v-avatar class="ma-3 school-card-avatar" tile >
-                <v-img :src="`${baseUrl}/asset/img/newIcon/闸机.png`" alt="postItem" ></v-img>
-            </v-avatar>
-            <div>
-                <p class="font-weight-black fs-15 mb-3"> {{lang.safeStudy}}  </p>
-                <div class="d-flex align-center">
-                <v-icon medium color="primary" class="mr-2">mdi-clock-outline </v-icon>
-                <p class="mb-0 mr-8">{{TimeView(contentData.created_at)}}</p>
-                <v-icon medium color="primary" class="mr-2">mdi-account </v-icon>
-                <p class="mb-0">{{contentData.users.name}}</p>
-                </div>
-            </div>
-            <!-- <div class="ml-auto">
-                <v-menu offset-y >
-                <template v-slot:activator="{ attrs, on }">
-                    <v-btn icon color="primary" v-bind="attrs" v-on="on" >
-                    <v-icon size="30">mdi-chevron-down </v-icon>
-                    </v-btn>
-                </template>
-                <v-list>
-                    <v-list-item link >
-                    <v-list-item-title class="px-2">{{lang.toTop}}</v-list-item-title>
-                    </v-list-item>
-                    <v-list-item link >
-                    <v-list-item-title class="px-2" @click="postRemove(contentData)">{{lang.remove}}</v-list-item-title>
-                    </v-list-item>
-                </v-list>
-                </v-menu>
-            </div> -->
-        </v-col>
+        <v-row class="justify-center align-center z-index-2 banner-custom ">
+            <v-icon size="70" @click="$router.go(-1)" class="position-absolute put-align-center" style="top:50%; left:20px">
+                mdi-chevron-left
+            </v-icon>
+            <h2 class="py-7">{{lang.safeStudy}}</h2>
+        </v-row>
+        <v-row class="pl-10 mt-5 ma-0">
+            <v-col cols="12">
+                <p class="text-wrap"><read-more more-str="全文" :text="shareData[0].text" link="#" less-str="收起" :max-chars="250"></read-more></p>
+            </v-col>
+            <v-col cols="12" v-if="checkIfAttachExist(shareData[0])">
+                <AttachItemViewer :items="shareData[0]" />
+            </v-col>
+        </v-row>
         <v-col cols="12" class="pl-10 pt-0">
             <v-row>
-                <v-col cols="12">
-                <p class="text-wrap"><read-more more-str="全文" :text="shareData[0].text" link="#" less-str="收起" :max-chars="250"></read-more></p>
-                </v-col>
-                <v-col cols="12" v-if="checkIfAttachExist(shareData[0])">
-                <AttachItemViewer :items="shareData[0]" />
-                </v-col>
+                
             </v-row>
         </v-col>
     </v-container>
