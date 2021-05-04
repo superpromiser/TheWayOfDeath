fd9cfec752049ea30158ddf0bae41039d6965d9b
smilecoding7 <superpromiser@gmail.com>
2021-05-04 21:25:38 +0800

    updated

--- resources/js/components/addMemberClass.vue
+++ resources/js/components/addMemberClass.vue
@@ -1,75 +1,75 @@
 <template>
-  <v-container>
-    <v-row class="ma-0 ">
-        <v-col cols="12" class="d-flex align-center justify-space-between">
-            <a @click="$router.go(-1)">
-                <v-icon size="70">
-                    mdi-chevron-left
-                </v-icon>
-            </a>
-            <span style="font-size:30px;line-height:2">{{title}}</span> 
-            <v-btn
-                tile
-                dark
-                color="#7879ff"
-                :loading="isSubmit"
-                @click="submit"
-                :disabled="noUser"
-            >
-                {{ lang.submit }}
-            </v-btn>
-        </v-col>
-    </v-row>
-    <v-divider light></v-divider>
-    <v-container >
-      <v-row class="ma-0" >
-        <v-col cols="12">
-          <v-checkbox
-            v-model="checkAll"
-            label="全选"
-            class="member-chk"
-            style="height:20px !important"
-            @click="selectAll"
-            color="#7879ff"
-            :disabled="noUser || isLoading"
-          ></v-checkbox>
-        </v-col>
-      </v-row>
-      <v-divider class="thick-border"></v-divider>
-    </v-container>
-    <v-container v-if="isLoading" class="d-flex justify-center align-center">
-        <v-progress-circular
-            indeterminate
-            color="primary"
-        ></v-progress-circular>
-    </v-container>
-    <v-container v-else-if="noUser" class="d-flex justify-center align-center">
-        <v-chip class="ma-2" color="primary" outlined pill >
-              没有数据
-            <v-icon right>
-            mdi-cancel 
-            </v-icon>
-        </v-chip>
-    </v-container>
-    <v-container v-else v-for="(user, index) in userList" :key="index" class="py-0">
-        <v-row class="ma-0">
-            <v-col cols="12" class="">
+    <v-container class="pa-0">
+        <v-container class="px-10 z-index-2 banner-custom">
+            <v-row>
+                <v-col cols="6" md="4" class="d-flex align-center position-relative">
+                    <a @click="$router.go(-1)">
+                        <v-icon size="70" class="left-24p">
+                            mdi-chevron-left
+                        </v-icon>
+                    </a>
+                </v-col>
+                <v-col cols="6" md="4" class="d-flex align-center justify-start justify-md-center">
+                    <h2>{{title}}</h2>
+                </v-col>
+                <v-col cols="12" md="4" class="d-flex align-center justify-end">
+                    <v-btn dark color="#49d29e" :loading="isSubmit" @click="submit" :disabled="noUser">
+                        {{ lang.submit }}
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+        <v-divider light></v-divider>
+        <v-container class="px-0">
+            <v-row class="ma-0 px-5" >
+                <v-col cols="12">
                 <v-checkbox
-                    class="mt-0 pt-0 ml-5"
-                    v-model="user.checkbox"
-                    @click="selectMem(user)"
-                    hide-details
-                    color="#7879ff"
-                >
-                    <template v-slot:label>
-                        <div class="d-flex">
-                            <v-avatar color="#7879ff" size="60" class="rounded-lg ml-3"  >
-                                <span v-if="user.avatar == '/'" class="white--text headline">{{user.name[0]}}</span>
-                                <v-img v-else :src="`${baseUrl}${user.avatar}`"></v-img>
-                            </v-avatar>
-                            <div class="ml-3">
-                                <p >{{user.name}}</p>
-                                <p class="mb-0">{{ pnEncrypt(user.phoneNumber) }}</p>
+                    v-model="checkAll"
+                    label="全选"
+                    class="member-chk"
+                    style="height:20px !important"
+                    @click="selectAll"
+                    color="#49d29e"
+                    :disabled="noUser || isLoading"
+                ></v-checkbox>
+                </v-col>
+            </v-row>
+            <v-divider class="thick-border"></v-divider>
+        </v-container>
+        <v-container v-if="isLoading" class="d-flex justify-center align-center">
+            <v-progress-circular
+                indeterminate
+                color="primary"
+            ></v-progress-circular>
+        </v-container>
+        <v-container v-else-if="noUser" class="d-flex justify-center align-center">
+            <v-chip class="ma-2" color="primary" outlined pill >
+                没有数据
+                <v-icon right>
+                mdi-cancel 
+                </v-icon>
+            </v-chip>
+        </v-container>
+        <v-container v-else v-for="(user, index) in userList" :key="index" class="pa-0">
+            <v-row class="ma-0 ">
+                <v-col cols="12" class="">
+                    <v-checkbox
+                        class="mt-0 pt-0 ml-5"
+                        v-model="user.checkbox"
+                        @click="selectMem(user)"
+                        hide-details
+                        color="#49d29e"
+                    >
+                        <template v-slot:label>
+                            <div class="d-flex">
+                                <v-avatar color="#49d29e" size="60" class="rounded-lg ml-3"  >
+                                    <span v-if="user.avatar == '/'" class="white--text headline">{{user.name[0]}}</span>
+                                    <v-img v-else :src="`${baseUrl}${user.avatar}`"></v-img>
+                                </v-avatar>
+                                <div class="ml-3">
+                                    <p >{{user.name}}</p>
+                                    <p class="mb-0">{{ pnEncrypt(user.phoneNumber) }}</p>
+                                </div>
                             </div>
                         </template>
                     </v-checkbox>
