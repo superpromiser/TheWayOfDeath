5003c9eb3ae08d4e1bbf5cbd296820eb600f5de3
W@ngD@nY@ng <59977368+wangdanyang711@users.noreply.github.com>
2021-05-11 05:11:29 -0300

    application school

--- resources/js/pages/class/applications/repair/index.vue
+++ resources/js/pages/class/applications/repair/index.vue
@@ -1,5 +1,101 @@
 <template>
-    <v-container class="pa-0">
+    <v-container class="pa-0" v-if="$isMobile()">
+        <v-container class="pt-0 px-0 h-100 bg-white mb-16 pb-10-px">
+            <v-row class="ma-0 bg-white justify-center position-sticky-top-0" >
+                <v-icon @click="$router.go(-1)" size="35" class="position-absolute put-align-center" style="left: 0px; top:50%" >
+                    mdi-chevron-left
+                </v-icon>
+                <p class="mb-0 font-size-0-95 font-weight-bold pa-3" >维修工单</p>
+                <v-btn @click="post" text color="#7879ff" class="position-absolute put-align-center" style="right: 0px; top:50%">
+                    发布
+                </v-btn>
+            </v-row>
+            <div class="cus-divider-light-gray-height"></div>
+            <v-row class="ma-0">
+                <v-col cols="12" class="py-0">
+                    <v-select
+                        :items="viewList"
+                        label="Solo field"
+                        single-line
+                        color="#7879ff"
+                        @change="selViewType"
+                        v-model="viewType"
+                        item-text='text'
+                        item-value="value"
+                        hide-details
+                    ></v-select>
+                </v-col>
+                <v-col cols="12" class="pt-0">
+                    <v-menu
+                        v-model="menu"
+                        :close-on-content-click="false"
+                        :nudge-right="40"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                    >
+                        <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                            color="#7879ff"
+                            v-model="date"
+                            prepend-icon="mdi-calendar"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                            hide-details
+                        ></v-text-field>
+                        </template>
+                        <v-date-picker
+                        v-model="date"
+                        @input="menu = false"
+                        @change="selDate"
+                        color="#7879ff"
+                        ></v-date-picker>
+                    </v-menu>
+                </v-col>
+            </v-row>
+            <v-container v-if="contentList.length" class="pa-0" v-for="content in contentList" :key="content.id" >
+                <v-row class="px-3 mt-1" v-if="content.repairdata">
+                    <RepairDataPost :content="content"></RepairDataPost>
+                    <FooterPost :footerInfo='content'></FooterPost>
+                </v-row>
+            </v-container>
+            <InfiniteLoading 
+                class="pb-3 w-100"
+                @infinite="infiniteHandler"
+            >   
+                <div slot="spinner" class="pa-3 ma-3 text-center">
+                    <v-progress-circular
+                        indeterminate
+                        color="#7879ff"
+                    ></v-progress-circular>
+                </div>
+                <div slot="no-more" class="pa-3 ma-3 text-center">
+                    <v-chip
+                    class="ma-2"
+                    color="#7879ff"
+                    outlined
+                    pill
+                    >
+                    暂无
+                    <v-icon right>
+                        mdi-cancel 
+                    </v-icon>
+                    </v-chip>
+                </div>
+                <div slot="no-results" class="position-relative row m-0 p-2 h-50 d-flex justify-content-center align-items-center">
+                    <div class="w-100 text-center p-5 m-5 mt-10">
+                        <v-icon size="60" color="grey darken-1">
+                            mdi-magnify
+                        </v-icon>
+                        <h5>暂无</h5>
+                    </div>
+                </div>
+            </InfiniteLoading>
+        </v-container>
+        
+    </v-container>
+    <v-container class="pa-0" v-else>
         <v-container class="banner-custom mb-10 z-index-2" color="white" sticky elevation="20">
             <v-row>
                 <v-col cols="12" class="justify-space-between d-flex ma-0 align-center">
--- resources/js/pages/class/applications/share/index.vue
+++ resources/js/pages/class/applications/share/index.vue
@@ -1,5 +1,57 @@
 <template>
-    <v-container class="pa-0">
+    <v-container class="pa-0" v-if="$isMobile()">
+        <v-container class="pt-0 px-0 h-100 bg-white mb-16 pb-10-px">
+            <v-row class="ma-0 bg-white justify-center position-sticky-top-0" >
+                <v-icon @click="$router.go(-1)" size="35" class="position-absolute put-align-center" style="left: 0px; top:50%" >
+                    mdi-chevron-left
+                </v-icon>
+                <p class="mb-0 font-size-0-95 font-weight-bold pa-3" >{{lang.share}}</p>
+                <v-btn @click="post" text color="#7879ff" class="position-absolute put-align-center" style="right: 0px; top:50%">
+                    发布
+                </v-btn>
+            </v-row>
+            <div class="cus-divider-light-gray-height"></div>
+            <v-container v-if="contentList.length" class="px-0" v-for="content in contentList" :key="content.id" >
+                <v-row class="px-3 mt-1">
+                    <SharePost :content="content"></SharePost>
+                    <FooterPost :footerInfo='content'></FooterPost>
+                </v-row>
+            </v-container>
+            <InfiniteLoading 
+                class="pb-3 w-100"
+                @infinite="infiniteHandler"
+            >   
+                <div slot="spinner" class="pa-3 ma-3 text-center">
+                    <v-progress-circular
+                        indeterminate
+                        color="#7879ff"
+                    ></v-progress-circular>
+                </div>
+                <div slot="no-more" class="pa-3 ma-3 text-center">
+                    <v-chip
+                    class="ma-2"
+                    color="#7879ff"
+                    outlined
+                    pill
+                    >
+                    暂无
+                    <v-icon right>
+                        mdi-cancel 
+                    </v-icon>
+                    </v-chip>
+                </div>
+                <div slot="no-results" class="position-relative row m-0 p-2 h-50 d-flex justify-content-center align-items-center">
+                    <div class="w-100 text-center p-5 m-5 mt-10">
+                        <v-icon size="60" color="grey darken-1">
+                            mdi-magnify
+                        </v-icon>
+                        <h5>暂无</h5>
+                    </div>
+                </div>
+            </InfiniteLoading>
+        </v-container>
+    </v-container>
+    <v-container class="pa-0" v-else>
         <v-container class="banner-custom z-index-2" color="white" sticky elevation="20">
             <v-row>
                 <v-col cols="12" class="justify-space-between d-flex ma-0 align-center">
