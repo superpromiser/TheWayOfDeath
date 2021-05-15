86e8687b016997e1a327dda9eddf848234bd804f
W@ngD@nY@ng <59977368+wangdanyang711@users.noreply.github.com>
2021-05-15 17:42:58 +0800

    share finished pattern

--- resources/js/pages/school/posts/share/new.vue
+++ resources/js/pages/school/posts/share/new.vue
@@ -266,6 +266,14 @@ export default {
             if(this.shareData.content.length == 0){
                 return this.$snackbar.showMessage({content: this.lang.share+this.lang.requireContent, color: "error"})
             }
+            if(this.shareData.publishType == 'spec'){
+                if(this.$isMobile()){
+                    this.$set(this.shareData, 'specUsers', this.publishSpecUserList);
+                }
+                else{
+                    
+                }
+            }
             this.isSubmit = true
             await createShare(this.shareData).then(res=>{
                 if(this.$isMobile()){
@@ -291,6 +299,7 @@ export default {
             }
             this.shareData.content.push(data)
         },
+
 
         selectPublishType( val ){
             console.log(val);
