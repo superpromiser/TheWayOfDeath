be9b5ed6384da70e2b39e0ebed9ecff565dbe255
smilecoding7 <superpromiser@gmail.com>
2021-05-17 04:20:20 +0800

    updated

--- resources/js/pages/details/schoolStory/index.vue
+++ resources/js/pages/details/schoolStory/index.vue
@@ -27,10 +27,10 @@
         </v-row>
         <v-row class="pl-10 mt-5 ma-0">
             <v-col cols="12">
-                <p class="text-wrap"><read-more more-str="全文" :text="schoolStory.text" link="#" less-str="收起" :max-chars="250"></read-more></p>
+                <p class="text-wrap"><read-more more-str="全文" :text="schoolStory[0].text" link="#" less-str="收起" :max-chars="250"></read-more></p>
             </v-col>
-            <v-col cols="12" v-if="checkIfAttachExist(schoolStory)">
-                <AttachItemViewer :items="schoolStory" />
+            <v-col cols="12" v-if="checkIfAttachExist(schoolStory[0])">
+                <AttachItemViewer :items="schoolStory[0]" />
             </v-col>
         </v-row>
     </v-container>
