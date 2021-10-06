57ffc4e74f74f474e9a9bb577f272b81f54aa59c
smilecoding7 <superpromiser@gmail.com>
2021-10-06 23:03:21 +0800

    updated

--- resources/js/api/alarm.js
+++ resources/js/api/alarm.js
@@ -1,34 +1,34 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getAlarm(){
-    return axios.get('/api/v1/alarm');
+function getAlarm() {
+  return axios.get("/api/v1/alarm");
 }
 
-function createAlarm(payload){
-    return axios.post('/api/v1/alarm',payload);
+function createAlarm(payload) {
+  return axios.post("/api/v1/alarm", payload);
 }
 
-function updateAlarm(payload){
-    return axios.put('/api/v1/alarm',payload);
+function updateAlarm(payload) {
+  return axios.put("/api/v1/alarm", payload);
 }
 
-function deleteAlarm(payload){
-    return axios.delete('/api/v1/alarm',{data:payload});
+function deleteAlarm(payload) {
+  return axios.delete("/api/v1/alarm", { data: payload });
 }
 
-function getReadCnt(payload){
-    return axios.get('/api/v1/readCnt',{params:payload})
+function getReadCnt(payload) {
+  return axios.get("/api/v1/readCnt", { params: payload });
 }
 
-function createReadCnt(payload){
-    return axios.post('/api/v1/readCnt',payload)
+function createReadCnt(payload) {
+  return axios.post("/api/v1/readCnt", payload);
 }
 
-export{
-    getAlarm,
-    createAlarm,
-    updateAlarm,
-    deleteAlarm,
-    getReadCnt,
-    createReadCnt
-}
+export {
+  getAlarm,
+  createAlarm,
+  updateAlarm,
+  deleteAlarm,
+  getReadCnt,
+  createReadCnt
+};
