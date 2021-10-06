aac69850ab631ce57e82c49c814441f0b99238b4
smilecoding7 <superpromiser@gmail.com>
2021-10-07 03:57:23 +0800

    updated

--- resources/js/api/bulletinBoard.js
+++ resources/js/api/bulletinBoard.js
@@ -1,24 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getBulletinBoard(payload){
-    return axios.get('/api/v1/bulletinBoard',{params:payload});
+function getBulletinBoard(payload) {
+  return axios.get("/api/v1/bulletinBoard", { params: payload });
 }
 
-function createBulletinBoard(payload){
-    return axios.post('/api/v1/bulletinBoard',payload);
+function createBulletinBoard(payload) {
+  return axios.post("/api/v1/bulletinBoard", payload);
 }
 
-function updateBulletinBoard(payload){
-    return axios.put('/api/v1/bulletinBoard',payload);
+function updateBulletinBoard(payload) {
+  return axios.put("/api/v1/bulletinBoard", payload);
 }
 
-function deleteBoard(payload){
-    return axios.delete('/api/v1/bulletinBoard',{data:payload});
+function deleteBoard(payload) {
+  return axios.delete("/api/v1/bulletinBoard", { data: payload });
 }
 
-export{
-    getBulletinBoard,
-    createBulletinBoard,
-    updateBulletinBoard,
-    deleteBoard,
-}
+export {
+  getBulletinBoard,
+  createBulletinBoard,
+  updateBulletinBoard,
+  deleteBoard
+};
