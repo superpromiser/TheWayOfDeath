aad1070a94fbf2696533d3cf7476f7819b66b366
smilecoding7 <superpromiser@gmail.com>
2021-10-06 23:02:52 +0800

    updated

--- resources/js/api/anouncement.js
+++ resources/js/api/anouncement.js
@@ -1,54 +1,54 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getAnouncement(payload){
-    return axios.get('/api/v1/anouncement',{params:payload});
+function getAnouncement(payload) {
+  return axios.get("/api/v1/anouncement", { params: payload });
 }
 
-function createAnouncement(payload){
-    return axios.post('/api/v1/anouncement',payload);
+function createAnouncement(payload) {
+  return axios.post("/api/v1/anouncement", payload);
 }
 
-function updateAnouncement(payload){
-    return axios.put('/api/v1/anouncement',payload);
+function updateAnouncement(payload) {
+  return axios.put("/api/v1/anouncement", payload);
 }
 
-function deleteAnouncement(payload){
-    return axios.delete('/api/v1/anouncement',{data:payload})
+function deleteAnouncement(payload) {
+  return axios.delete("/api/v1/anouncement", { data: payload });
 }
 
-function getTemplateCnt(payload){
-    return axios.get('/api/v1/anouncement/templateCnt',{params:payload})
+function getTemplateCnt(payload) {
+  return axios.get("/api/v1/anouncement/templateCnt", { params: payload });
 }
 
-function getTemplateList(payload){
-    return axios.get('/api/v1/anouncement/template',{params:payload})
+function getTemplateList(payload) {
+  return axios.get("/api/v1/anouncement/template", { params: payload });
 }
 
-function createTemplate(payload){
-    return axios.post('/api/v1/anouncement/template',payload)
+function createTemplate(payload) {
+  return axios.post("/api/v1/anouncement/template", payload);
 }
 
-function deleteTemplate(payload){
-    return axios.delete('/api/v1/anouncement/template',{data:payload})
+function deleteTemplate(payload) {
+  return axios.delete("/api/v1/anouncement/template", { data: payload });
 }
 
-function getSchoolUsers(payload){
-    return axios.get('/api/v1/schoolUsers',{params:payload})
+function getSchoolUsers(payload) {
+  return axios.get("/api/v1/schoolUsers", { params: payload });
 }
 
-function getLessonUsers(payload){
-    return axios.get('/api/v1/lessonUsers',{params:payload})
+function getLessonUsers(payload) {
+  return axios.get("/api/v1/lessonUsers", { params: payload });
 }
 
-export{
-    getAnouncement,
-    createAnouncement,
-    updateAnouncement,
-    deleteAnouncement,
-    getTemplateCnt,
-    getTemplateList,
-    createTemplate,
-    deleteTemplate,
-    getSchoolUsers,
-    getLessonUsers,
-}
+export {
+  getAnouncement,
+  createAnouncement,
+  updateAnouncement,
+  deleteAnouncement,
+  getTemplateCnt,
+  getTemplateList,
+  createTemplate,
+  deleteTemplate,
+  getSchoolUsers,
+  getLessonUsers
+};
