42b68a968337081b4544b96e6337f3231ca4bd95
smilecoding7 <superpromiser@gmail.com>
2021-10-07 03:57:40 +0800

    updated

--- resources/js/api/campus.js
+++ resources/js/api/campus.js
@@ -1,24 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getCampus(payload){
-    return axios.get('/api/v1/campus',{params:payload});
+function getCampus(payload) {
+  return axios.get("/api/v1/campus", { params: payload });
 }
 
-function createCampus(payload){
-    return axios.post('/api/v1/campus',payload);
+function createCampus(payload) {
+  return axios.post("/api/v1/campus", payload);
 }
 
-function updateCampus(payload){
-    return axios.put('/api/v1/campus',payload);
+function updateCampus(payload) {
+  return axios.put("/api/v1/campus", payload);
 }
 
-function deleteCampus(payload){
-    return axios.delete('/api/v1/campus',{data:payload});
+function deleteCampus(payload) {
+  return axios.delete("/api/v1/campus", { data: payload });
 }
 
-export{
-    getCampus,
-    createCampus,
-    updateCampus,
-    deleteCampus
-}
+export { getCampus, createCampus, updateCampus, deleteCampus };
