ea9c45954e41a70a0d39b4daf538f8db1fd2a8bf
smilecoding7 <superpromiser@gmail.com>
2021-10-08 03:49:44 +0800

    updated

--- resources/js/api/checkIn.js
+++ resources/js/api/checkIn.js
@@ -1,24 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getCheckInData(payload){
-    return axios.get('/api/v1/checkIn',{params:payload});
+function getCheckInData(payload) {
+  return axios.get("/api/v1/checkIn", { params: payload });
 }
 
-function createCheckInData(payload){
-    return axios.post('/api/v1/checkIn',payload)
+function createCheckInData(payload) {
+  return axios.post("/api/v1/checkIn", payload);
 }
 
-function updateCheckInData(payload){
-    return axios.put('/api/v1/checkIn',payload)
+function updateCheckInData(payload) {
+  return axios.put("/api/v1/checkIn", payload);
 }
 
-function deleteCheckInData(payload){
-    return axios.delete('/api/v1/checkIn',{data:payload})
+function deleteCheckInData(payload) {
+  return axios.delete("/api/v1/checkIn", { data: payload });
 }
 
-export{
-    getCheckInData,
-    createCheckInData,
-    updateCheckInData,
-    deleteCheckInData
-}
+export {
+  getCheckInData,
+  createCheckInData,
+  updateCheckInData,
+  deleteCheckInData
+};
