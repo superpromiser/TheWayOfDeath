55ad1e6b9fc73ba40bfd1829c27f7eeff7c45000
smilecoding7 <superpromiser@gmail.com>
2021-10-08 03:50:17 +0800

    updated

--- resources/js/api/class.js
+++ resources/js/api/class.js
@@ -1,30 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getClass(){
-    return axios.get('/api/v1/class');
+function getClass() {
+  return axios.get("/api/v1/class");
 }
 
-function createClass(payload){
-    return axios.post('/api/v1/class',payload)
+function createClass(payload) {
+  return axios.post("/api/v1/class", payload);
 }
 
-function updateClass(payload){
-    return axios.put('/api/v1/class',payload)
+function updateClass(payload) {
+  return axios.put("/api/v1/class", payload);
 }
 
-function deleteClass(payload){
-    return axios.delete('/api/v1/class',{data:payload})
+function deleteClass(payload) {
+  return axios.delete("/api/v1/class", { data: payload });
 }
 
-function getGradeInfo(payload){
-
-    return axios.get('/api/v1/gradeName',{params:payload})
+function getGradeInfo(payload) {
+  return axios.get("/api/v1/gradeName", { params: payload });
 }
 
-export{
-    getClass,
-    createClass,
-    updateClass,
-    deleteClass,
-    getGradeInfo,
-}
+export { getClass, createClass, updateClass, deleteClass, getGradeInfo };
