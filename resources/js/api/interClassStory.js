b1d6cb8601e30b16d7ec5e70aeaf48886b1c9353
smilecoding7 <superpromiser@gmail.com>
2021-10-09 04:31:23 +0800

    updated

--- resources/js/api/interClassStory.js
+++ resources/js/api/interClassStory.js
@@ -1,51 +1,49 @@
-import axios from 'axios'
+import axios from "axios";
 
-function getInterClassStory(payload){
-    return axios.get('/api/v1/interClassStory',{params:payload})
+function getInterClassStory(payload) {
+  return axios.get("/api/v1/interClassStory", { params: payload });
 }
 
-function createInterClassStory(payload){
-    return axios.post('/api/v1/interClassStory',payload)
+function createInterClassStory(payload) {
+  return axios.post("/api/v1/interClassStory", payload);
 }
 
-function updateInterClassStory(payload){
-    return axios.put('/api/v1/interClassStory',payload)
+function updateInterClassStory(payload) {
+  return axios.put("/api/v1/interClassStory", payload);
 }
 
-function deleteInterClassStory(payload){
-    return axios.delete('/api/v1/interClassStory',{data:payload})
+function deleteInterClassStory(payload) {
+  return axios.delete("/api/v1/interClassStory", { data: payload });
 }
 
-function getTemplateCnt(payload){
-    return axios.get('/api/v1/interClassStory/templateCnt',{params:payload})
+function getTemplateCnt(payload) {
+  return axios.get("/api/v1/interClassStory/templateCnt", { params: payload });
 }
 
-function getTemplateList(payload){
-    return axios.get("/api/v1/interClassStory/template",{params:payload})
+function getTemplateList(payload) {
+  return axios.get("/api/v1/interClassStory/template", { params: payload });
 }
 
-function createTemplate(payload){
-    return axios.post('/api/v1/interClassStory/template',payload)
+function createTemplate(payload) {
+  return axios.post("/api/v1/interClassStory/template", payload);
 }
 
-function updateTemplate(payload){
-    return axios.put('/api/v1/interClassSotry/template',payload)
+function updateTemplate(payload) {
+  return axios.put("/api/v1/interClassSotry/template", payload);
 }
 
-
-function deleteTemplate(payload){
-    return axios.delete('/api/v1/interClassStory/template',{data:payload})
+function deleteTemplate(payload) {
+  return axios.delete("/api/v1/interClassStory/template", { data: payload });
 }
 
-
-export{
-    getInterClassStory,
-    createInterClassStory,
-    updateInterClassStory,
-    deleteInterClassStory,
-    getTemplateCnt,
-    getTemplateList,
-    createTemplate,
-    updateTemplate,
-    deleteTemplate,
-}
+export {
+  getInterClassStory,
+  createInterClassStory,
+  updateInterClassStory,
+  deleteInterClassStory,
+  getTemplateCnt,
+  getTemplateList,
+  createTemplate,
+  updateTemplate,
+  deleteTemplate
+};
