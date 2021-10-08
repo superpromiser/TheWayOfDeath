fbcb8eca7645ab20c9db67651526ef1c3ab731b0
smilecoding7 <superpromiser@gmail.com>
2021-10-09 04:30:56 +0800

    updated

--- resources/js/api/homework.js
+++ resources/js/api/homework.js
@@ -1,60 +1,59 @@
-import axios from 'axios'
+import axios from "axios";
 
-function getHomeworkData(payload){
-    return axios.get('/api/v1/homeworkData',{params:payload})
+function getHomeworkData(payload) {
+  return axios.get("/api/v1/homeworkData", { params: payload });
 }
 
-function createHomeworkData(payload){
-    return axios.post('/api/v1/homeworkData',payload)
+function createHomeworkData(payload) {
+  return axios.post("/api/v1/homeworkData", payload);
 }
 
-function getHomeworkResult(payload){
-    return axios.get('/api/v1/homeworkResult',{params:payload})
+function getHomeworkResult(payload) {
+  return axios.get("/api/v1/homeworkResult", { params: payload });
 }
 
-function createHomeworkResult(payload){
-    return axios.post('/api/v1/homeworkResult',payload)
+function createHomeworkResult(payload) {
+  return axios.post("/api/v1/homeworkResult", payload);
 }
 
-function getAppHomeworkData(payload){
-    return axios.get('/api/v1/appHomeworkData',{params:payload})
+function getAppHomeworkData(payload) {
+  return axios.get("/api/v1/appHomeworkData", { params: payload });
 }
 
-function getMySubject(payload){
-    return axios.get('/api/v1/mySubject',{params:payload})
+function getMySubject(payload) {
+  return axios.get("/api/v1/mySubject", { params: payload });
 }
 
-function getTemplateCnt(payload){
-    return axios.get('/api/v1/homeworkData/templateCnt',{params:payload})
+function getTemplateCnt(payload) {
+  return axios.get("/api/v1/homeworkData/templateCnt", { params: payload });
 }
 
-
-function getTemplateList(payload){
-    return axios.get('/api/v1/homeworkData/template',{params:payload})
+function getTemplateList(payload) {
+  return axios.get("/api/v1/homeworkData/template", { params: payload });
 }
 
-function createTemplate(payload){
-    return axios.post('/api/v1/homeworkData/template',payload)
+function createTemplate(payload) {
+  return axios.post("/api/v1/homeworkData/template", payload);
 }
 
-function updateTemplate(payload){
-    return axios.put('/api/v1/homeworkData/template',payload)
+function updateTemplate(payload) {
+  return axios.put("/api/v1/homeworkData/template", payload);
 }
 
-function deleteTemplate(payload){
-    return axios.delete('/api/v1/homeworkData/template',{data:payload})
+function deleteTemplate(payload) {
+  return axios.delete("/api/v1/homeworkData/template", { data: payload });
 }
 
-export{
-    getHomeworkData,
-    createHomeworkData,
-    getHomeworkResult,
-    createHomeworkResult,
-    getAppHomeworkData,
-    getMySubject,
-    getTemplateCnt,
-    getTemplateList,
-    createTemplate,
-    updateTemplate,
-    deleteTemplate
-}
+export {
+  getHomeworkData,
+  createHomeworkData,
+  getHomeworkResult,
+  createHomeworkResult,
+  getAppHomeworkData,
+  getMySubject,
+  getTemplateCnt,
+  getTemplateList,
+  createTemplate,
+  updateTemplate,
+  deleteTemplate
+};
