897bb1c7734a34bc5ed6d6ec794ed4a3def47e1b
smilecoding7 <superpromiser@gmail.com>
2021-10-10 01:34:09 +0800

    updated

--- resources/js/api/notification.js
+++ resources/js/api/notification.js
@@ -1,25 +1,25 @@
-import axios from 'axios'
+import axios from "axios";
 
-function getNotification(){
-    return axios.get('/api/v1/class/notification');
+function getNotification() {
+  return axios.get("/api/v1/class/notification");
 }
 
-function createNotification(payload){
-    //console.log('+++++++++',payload)
-    return axios.post('/api/v1/class/notification',payload);
+function createNotification(payload) {
+  //console.log('+++++++++',payload)
+  return axios.post("/api/v1/class/notification", payload);
 }
 
-function updateNotification(payload){
-    return axios.put('/api/v1/class/notification',payload);
+function updateNotification(payload) {
+  return axios.put("/api/v1/class/notification", payload);
 }
 
-function deleteNotification(payload){
-    return axios.delete('/api/v1/class/notification',{data:payload})
+function deleteNotification(payload) {
+  return axios.delete("/api/v1/class/notification", { data: payload });
 }
 
-export{
-    getNotification,
-    createNotification,
-    updateNotification,
-    deleteNotification
-}
+export {
+  getNotification,
+  createNotification,
+  updateNotification,
+  deleteNotification
+};
