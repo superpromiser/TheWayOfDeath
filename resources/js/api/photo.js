089811f24c7acbc76d3e55d5d2aab4251d4b4b39
smilecoding7 <superpromiser@gmail.com>
2021-10-10 01:34:22 +0800

    updated

--- resources/js/api/photo.js
+++ resources/js/api/photo.js
@@ -1,14 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getClassPhoto(payload){
-    return axios.get('/api/v1/class/photos',{params:{classId:payload}})
+function getClassPhoto(payload) {
+  return axios.get("/api/v1/class/photos", { params: { classId: payload } });
 }
 
-function getClassFile(payload){
-    return axios.get('/api/v1/class/files',{params:{classId:payload}})
+function getClassFile(payload) {
+  return axios.get("/api/v1/class/files", { params: { classId: payload } });
 }
 
-export{
-    getClassPhoto,
-    getClassFile,
-}
+export { getClassPhoto, getClassFile };
