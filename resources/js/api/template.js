83a774902d96d740f1b207252467e3d70ead574a
smilecoding7 <superpromiser@gmail.com>
2021-10-12 00:15:24 +0800

    updated

--- resources/js/api/template.js
+++ resources/js/api/template.js
@@ -1,25 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getTemplate(payload){
-    //console.log('getTemplate',payload)
-    return axios.get('/api/v1/template',{params:payload});
+function getTemplate(payload) {
+  //console.log('getTemplate',payload)
+  return axios.get("/api/v1/template", { params: payload });
 }
 
-function createTemplate(payload){
-    return axios.post('/api/v1/template',payload);
+function createTemplate(payload) {
+  return axios.post("/api/v1/template", payload);
 }
 
-function updateTemplate(payload){
-    return axios.put('/api/v1/template',payload);
+function updateTemplate(payload) {
+  return axios.put("/api/v1/template", payload);
 }
 
-function deleteTemplate(payload){
-    return axios.delete('/api/v1/template',{data:payload});
+function deleteTemplate(payload) {
+  return axios.delete("/api/v1/template", { data: payload });
 }
 
-export{
-    getTemplate,
-    createTemplate,
-    updateTemplate,
-    deleteTemplate
-}
+export { getTemplate, createTemplate, updateTemplate, deleteTemplate };
