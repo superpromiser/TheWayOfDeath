51ca067c9d733972310e66088e1d5dbd0fd6493d
smilecoding7 <superpromiser@gmail.com>
2021-10-11 12:52:14 +0800

    updated

--- resources/js/api/questionnaire.js
+++ resources/js/api/questionnaire.js
@@ -1,49 +1,49 @@
-import axios from 'axios'
+import axios from "axios";
 
-function getQuestionnaire(payload){
-    return axios.get('/api/v1/questionnaire',{params:payload});
+function getQuestionnaire(payload) {
+  return axios.get("/api/v1/questionnaire", { params: payload });
 }
 
-function createQuestionnaire(payload){
-    return axios.post('/api/v1/questionnaire',payload)
+function createQuestionnaire(payload) {
+  return axios.post("/api/v1/questionnaire", payload);
 }
 
-function updateQuestionnaire(payload){
-    return axios.put('/api/v1/questionnaire',payload);
+function updateQuestionnaire(payload) {
+  return axios.put("/api/v1/questionnaire", payload);
 }
 
-function deleteQuestionnaire(payload){
-    return axios.delete('/api/v1/questionnaire',{data: payload})
+function deleteQuestionnaire(payload) {
+  return axios.delete("/api/v1/questionnaire", { data: payload });
 }
 
-function getQuestionnaireTemp(payload){
-    return axios.get('/api/v1/questionnaireTemp',{params:payload});
+function getQuestionnaireTemp(payload) {
+  return axios.get("/api/v1/questionnaireTemp", { params: payload });
 }
 
-function createQuestionnaireTemp(payload){
-    return axios.post('/api/v1/questionnaireTemp',payload);
+function createQuestionnaireTemp(payload) {
+  return axios.post("/api/v1/questionnaireTemp", payload);
 }
 
-function updateQuestionnaireTemp(payload){
-    return axios.put('/api/v1/questionnaireTemp',payload);
+function updateQuestionnaireTemp(payload) {
+  return axios.put("/api/v1/questionnaireTemp", payload);
 }
 
-function deleteQuestionnaireTemp(payload){
-    return axios.delete('/api/v1/questionnaireTemp',{data:payload})
+function deleteQuestionnaireTemp(payload) {
+  return axios.delete("/api/v1/questionnaireTemp", { data: payload });
 }
 
-function getQuestionnaireTempCnt(payload){
-    return axios.get('/api/v1/questionnaireTempCnt',{params:payload});
+function getQuestionnaireTempCnt(payload) {
+  return axios.get("/api/v1/questionnaireTempCnt", { params: payload });
 }
 
-export{
-    getQuestionnaire,
-    createQuestionnaire,
-    updateQuestionnaire,
-    deleteQuestionnaire,
-    getQuestionnaireTemp,
-    createQuestionnaireTemp,
-    updateQuestionnaireTemp,
-    deleteQuestionnaireTemp,
-    getQuestionnaireTempCnt,
-}
+export {
+  getQuestionnaire,
+  createQuestionnaire,
+  updateQuestionnaire,
+  deleteQuestionnaire,
+  getQuestionnaireTemp,
+  createQuestionnaireTemp,
+  updateQuestionnaireTemp,
+  deleteQuestionnaireTemp,
+  getQuestionnaireTempCnt
+};
