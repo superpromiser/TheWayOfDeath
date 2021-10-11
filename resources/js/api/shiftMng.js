d2a68d72a42917fa5c0f80089cefcb204a7f2713
smilecoding7 <superpromiser@gmail.com>
2021-10-12 00:14:09 +0800

    updated

--- resources/js/api/shiftMng.js
+++ resources/js/api/shiftMng.js
@@ -1,44 +1,44 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getShiftMng(payload){
-    return axios.get('/api/v1/shiftMng',{params:payload})
+function getShiftMng(payload) {
+  return axios.get("/api/v1/shiftMng", { params: payload });
 }
 
-function createShiftMng(payload){
-    return axios.post('/api/v1/shiftMng',payload)
+function createShiftMng(payload) {
+  return axios.post("/api/v1/shiftMng", payload);
 }
 
-function updateShiftMng(payload){
-    return axios.put('/api/v1/shiftMng',payload)
+function updateShiftMng(payload) {
+  return axios.put("/api/v1/shiftMng", payload);
 }
 
-function deleteShifMng(payload){
-    return axios.delete('/api/v1/shiftMng',payload)
+function deleteShifMng(payload) {
+  return axios.delete("/api/v1/shiftMng", payload);
 }
 
-function getTemplateCnt(payload){
-    return axios.get('/api/v1/shiftMng/templateCnt',{params:payload})
+function getTemplateCnt(payload) {
+  return axios.get("/api/v1/shiftMng/templateCnt", { params: payload });
 }
 
-function getTemplateList(payload){
-    return axios.get('/api/v1/shifMng/template',{params:payload})
+function getTemplateList(payload) {
+  return axios.get("/api/v1/shifMng/template", { params: payload });
 }
 
-function createTemplate(payload){
-    return axios.post('/api/v1/shiftMng/template',payload)
+function createTemplate(payload) {
+  return axios.post("/api/v1/shiftMng/template", payload);
 }
 
-function deleteTemplate(payload){
-    return axios.delete('/api/v1/shiftMng/template',{data:payload})
+function deleteTemplate(payload) {
+  return axios.delete("/api/v1/shiftMng/template", { data: payload });
 }
 
-export{
-    getShiftMng,
-    createShiftMng,
-    updateShiftMng,
-    deleteShifMng,
-    getTemplateCnt,
-    getTemplateList,
-    createTemplate,
-    deleteTemplate
-}
+export {
+  getShiftMng,
+  createShiftMng,
+  updateShiftMng,
+  deleteShifMng,
+  getTemplateCnt,
+  getTemplateList,
+  createTemplate,
+  deleteTemplate
+};
