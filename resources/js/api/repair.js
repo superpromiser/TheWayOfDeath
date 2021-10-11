955cce12338dc598d161fc8f932db4c71994c0bf
smilecoding7 <superpromiser@gmail.com>
2021-10-11 12:53:17 +0800

    updated

--- resources/js/api/repair.js
+++ resources/js/api/repair.js
@@ -1,47 +1,47 @@
-import axios from 'axios'
+import axios from "axios";
 
-function getRepairData(payload){
-    return axios.get('/api/v1/repairData',{params:payload})
-}
-
-function createRepairData(payload){
-    return axios.post('/api/v1/repairData',payload)
+function getRepairData(payload) {
+  return axios.get("/api/v1/repairData", { params: payload });
 }
 
-function updateRepairData(payload){
-    return axios.put('/api/v1/repairData',payload)
+function createRepairData(payload) {
+  return axios.post("/api/v1/repairData", payload);
 }
 
-function deleteRepairData(payload){
-    return axios.delete('/api/v1/repairData',{data:payload})
-}
-function getTemplateCnt(payload){
-    return axios.get('/api/v1/repairData/templateCnt',{params:payload})
+function updateRepairData(payload) {
+  return axios.put("/api/v1/repairData", payload);
 }
 
-function getTemplateList(payload){
-    return axios.get('/api/v1/repairData/template',{params:payload})
+function deleteRepairData(payload) {
+  return axios.delete("/api/v1/repairData", { data: payload });
+}
+function getTemplateCnt(payload) {
+  return axios.get("/api/v1/repairData/templateCnt", { params: payload });
 }
 
-function createTemplate(payload){
-    return axios.post('/api/v1/repairData/template',payload)
+function getTemplateList(payload) {
+  return axios.get("/api/v1/repairData/template", { params: payload });
 }
 
-function updateTemplate(payload){
-    return axios.put('/api/v1/repairData/template',payload)
+function createTemplate(payload) {
+  return axios.post("/api/v1/repairData/template", payload);
 }
 
-function deleteTemplate(payload){
-    return axios.delete('/api/v1/repairData/template',{data:payload})
+function updateTemplate(payload) {
+  return axios.put("/api/v1/repairData/template", payload);
 }
-export{
-    getRepairData,
-    createRepairData,
-    updateRepairData,
-    deleteRepairData,
-    getTemplateList,
-    getTemplateCnt,
-    createTemplate,
-    updateTemplate,
-    deleteTemplate
+
+function deleteTemplate(payload) {
+  return axios.delete("/api/v1/repairData/template", { data: payload });
 }
+export {
+  getRepairData,
+  createRepairData,
+  updateRepairData,
+  deleteRepairData,
+  getTemplateList,
+  getTemplateCnt,
+  createTemplate,
+  updateTemplate,
+  deleteTemplate
+};
