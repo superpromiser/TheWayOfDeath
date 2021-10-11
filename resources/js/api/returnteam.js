9e01b70facb543e0734d32751b7a276d8bfc8995
smilecoding7 <superpromiser@gmail.com>
2021-10-11 12:53:41 +0800

    updated

--- resources/js/api/returnteam.js
+++ resources/js/api/returnteam.js
@@ -1,29 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getReturnTeam(){
-    return axios.get('/api/v1/returnteam');
+function getReturnTeam() {
+  return axios.get("/api/v1/returnteam");
 }
 
-function createReturnTeam(payload){
-    return axios.post('/api/v1/returnteam',payload);
+function createReturnTeam(payload) {
+  return axios.post("/api/v1/returnteam", payload);
 }
 
-function createRemainTeam(payload){
-    return axios.post('/api/v1/remainteam',payload);
+function createRemainTeam(payload) {
+  return axios.post("/api/v1/remainteam", payload);
 }
 
-function updateReturnTeam(payload){
-    return axios.put('/api/v1/returnteam',payload);
+function updateReturnTeam(payload) {
+  return axios.put("/api/v1/returnteam", payload);
 }
 
-function deleteReturnTeam(payload){
-    return axios.delete('/api/v1/returnteam',{data:payload});
+function deleteReturnTeam(payload) {
+  return axios.delete("/api/v1/returnteam", { data: payload });
 }
 
-export{
-    getReturnTeam,
-    createReturnTeam,
-    createRemainTeam,
-    updateReturnTeam,
-    deleteReturnTeam,
-}
+export {
+  getReturnTeam,
+  createReturnTeam,
+  createRemainTeam,
+  updateReturnTeam,
+  deleteReturnTeam
+};
