7300a0d7e2935f5bf30292879b593f887cbd0195
smilecoding7 <superpromiser@gmail.com>
2021-10-11 12:52:31 +0800

    updated

--- resources/js/api/recognition.js
+++ resources/js/api/recognition.js
@@ -1,24 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getRecognition(payload){
-    return axios.get('/api/v1/class/recognition',{params:payload});
+function getRecognition(payload) {
+  return axios.get("/api/v1/class/recognition", { params: payload });
 }
 
-function createRecognition(payload){
-    return axios.post('/api/v1/class/recognition',payload);
+function createRecognition(payload) {
+  return axios.post("/api/v1/class/recognition", payload);
 }
 
-function updateRecognition(payload){
-    return axios.put('/api/v1/class/recognition',payload);
+function updateRecognition(payload) {
+  return axios.put("/api/v1/class/recognition", payload);
 }
 
-function deleteRecognition(payload){
-    return axios.delete('/api/v1/class/recognition',{data:payload});
+function deleteRecognition(payload) {
+  return axios.delete("/api/v1/class/recognition", { data: payload });
 }
 
-export{
-    getRecognition,
-    createRecognition,
-    updateRecognition,
-    deleteRecognition,
-}
+export {
+  getRecognition,
+  createRecognition,
+  updateRecognition,
+  deleteRecognition
+};
