14872a457ecb1fab31a01079caae12e100d4a35b
smilecoding7 <superpromiser@gmail.com>
2021-10-11 12:55:17 +0800

    updated

--- resources/js/api/school.js
+++ resources/js/api/school.js
@@ -1,48 +1,45 @@
-import axios from 'axios'
-
+import axios from "axios";
 
 //school API
-function createSchool(payload){
-    return axios.post('/api/v1/school', payload);
+function createSchool(payload) {
+  return axios.post("/api/v1/school", payload);
 }
-function updateSchool(payload){
-    return axios.put('/api/v1/school', payload);
+function updateSchool(payload) {
+  return axios.put("/api/v1/school", payload);
 }
-function getSchool(payload){
-    return axios.get('/api/v1/school', payload);
+function getSchool(payload) {
+  return axios.get("/api/v1/school", payload);
 }
-function deleteSchool(payload){
-    return axios.delete('/api/v1/school', {data : payload});
+function deleteSchool(payload) {
+  return axios.delete("/api/v1/school", { data: payload });
 }
 
 //school Manager API
-function createSchoolManager(payload){
-    return axios.post('/api/v1/school/manager', payload);
+function createSchoolManager(payload) {
+  return axios.post("/api/v1/school/manager", payload);
 }
-function updateSchoolManager(payload){
-    return axios.put('/api/v1/school/manager', payload);
+function updateSchoolManager(payload) {
+  return axios.put("/api/v1/school/manager", payload);
 }
-function getSchoolManager(payload){
-    return axios.get('/api/v1/school/manager', payload);
+function getSchoolManager(payload) {
+  return axios.get("/api/v1/school/manager", payload);
 }
-function deleteSchoolManager(payload){
-    return axios.delete('/api/v1/school/manager', {data : payload});
-}
-
-function getSchoolTree(){
-    return axios.get('/api/v1/school/tree');
+function deleteSchoolManager(payload) {
+  return axios.delete("/api/v1/school/manager", { data: payload });
 }
 
-
-export{
-    createSchool,
-    updateSchool,
-    getSchool,
-    deleteSchool,
-    createSchoolManager,
-    updateSchoolManager,
-    getSchoolManager,
-    deleteSchoolManager,
-    getSchoolTree,
-    
+function getSchoolTree() {
+  return axios.get("/api/v1/school/tree");
 }
+
+export {
+  createSchool,
+  updateSchool,
+  getSchool,
+  deleteSchool,
+  createSchoolManager,
+  updateSchoolManager,
+  getSchoolManager,
+  deleteSchoolManager,
+  getSchoolTree
+};
