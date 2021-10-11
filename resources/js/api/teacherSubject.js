48e974745a0120a681e1d0cad35154328bb42a86
smilecoding7 <superpromiser@gmail.com>
2021-10-12 00:15:04 +0800

    updated

--- resources/js/api/teacherSubject.js
+++ resources/js/api/teacherSubject.js
@@ -1,24 +1,24 @@
-import axios from 'axios'
+import axios from "axios";
 
-function getScheduleClass(){
-    return axios.get('/api/v1/teacher/schedule');
+function getScheduleClass() {
+  return axios.get("/api/v1/teacher/schedule");
 }
 
-function createScheduleClass(payload){
-    return axios.post('/api/v1/teacher/schedule',payload)
+function createScheduleClass(payload) {
+  return axios.post("/api/v1/teacher/schedule", payload);
 }
 
-function updateScheduleClass(payload){
-    return axios.put('/api/v1/teacher/schedule',payload)
+function updateScheduleClass(payload) {
+  return axios.put("/api/v1/teacher/schedule", payload);
 }
 
-function deleteScheduleClass(payload){
-    return axios.delete('/api/v1/teacher/schedule',payload)
+function deleteScheduleClass(payload) {
+  return axios.delete("/api/v1/teacher/schedule", payload);
 }
 
-export{
-    getScheduleClass,
-    createScheduleClass,
-    updateScheduleClass,
-    deleteScheduleClass,
-}
+export {
+  getScheduleClass,
+  createScheduleClass,
+  updateScheduleClass,
+  deleteScheduleClass
+};
