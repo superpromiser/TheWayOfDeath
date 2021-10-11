146c95c419280eb4eb673414057e21a6a31a0b40
smilecoding7 <superpromiser@gmail.com>
2021-10-11 12:54:03 +0800

    updated

--- resources/js/api/safeStudy.js
+++ resources/js/api/safeStudy.js
@@ -1,50 +1,49 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getSafeStudy(payload){
-    return axios.get('/api/v1/safeStudy',{params:payload})
+function getSafeStudy(payload) {
+  return axios.get("/api/v1/safeStudy", { params: payload });
 }
 
-function createSafeStudy(payload){
-    return axios.post('/api/v1/safeStudy',payload)
+function createSafeStudy(payload) {
+  return axios.post("/api/v1/safeStudy", payload);
 }
 
-function updateSafeStudy(payload){
-    return axios.put('/api/v1/safeStudy',payload)
+function updateSafeStudy(payload) {
+  return axios.put("/api/v1/safeStudy", payload);
 }
 
-function deleteSafeStudy(payload){
-    return axios.delete('/api/v1/safeStudy',{data:payload})
+function deleteSafeStudy(payload) {
+  return axios.delete("/api/v1/safeStudy", { data: payload });
 }
 
-function getTemplateCnt(payload){
-    return axios.get('/api/v1/safeStudy/templateCnt',{params:payload})
+function getTemplateCnt(payload) {
+  return axios.get("/api/v1/safeStudy/templateCnt", { params: payload });
 }
 
-function getTemplateList(payload){
-    return axios.get('/api/v1/safeStudy/template',{params:payload})
+function getTemplateList(payload) {
+  return axios.get("/api/v1/safeStudy/template", { params: payload });
 }
 
-function createTemplate(payload){
-    return axios.post('/api/v1/safeStudy/template',payload)
-
+function createTemplate(payload) {
+  return axios.post("/api/v1/safeStudy/template", payload);
 }
 
-function updateTemplate(payload){
-    return axios.put('/api/v1/safeStudy/template',payload)
+function updateTemplate(payload) {
+  return axios.put("/api/v1/safeStudy/template", payload);
 }
 
-function deleteTemplate(payload){
-    return axios.delete('/api/v1/safeStudy/template',{data:payload})
+function deleteTemplate(payload) {
+  return axios.delete("/api/v1/safeStudy/template", { data: payload });
 }
 
-export{
-    getSafeStudy,
-    createSafeStudy,
-    updateSafeStudy,
-    deleteSafeStudy,
-    getTemplateCnt,
-    getTemplateList,
-    createTemplate,
-    updateTemplate,
-    deleteTemplate
-}
+export {
+  getSafeStudy,
+  createSafeStudy,
+  updateSafeStudy,
+  deleteSafeStudy,
+  getTemplateCnt,
+  getTemplateList,
+  createTemplate,
+  updateTemplate,
+  deleteTemplate
+};
