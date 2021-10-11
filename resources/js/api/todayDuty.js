fb53077a6105aded9cba4085f1d08c190e269040
smilecoding7 <superpromiser@gmail.com>
2021-10-12 00:16:06 +0800

    updated

--- resources/js/api/todayDuty.js
+++ resources/js/api/todayDuty.js
@@ -1,24 +1,24 @@
-import axios from 'axios'
+import axios from "axios";
 
-function getTodayDutyData(payload){
-    return axios.get('/api/v1/todayDuty',{params:payload})
+function getTodayDutyData(payload) {
+  return axios.get("/api/v1/todayDuty", { params: payload });
 }
 
-function createTodayDutyData(payload){
-    return axios.post('/api/v1/todayDuty',payload)
+function createTodayDutyData(payload) {
+  return axios.post("/api/v1/todayDuty", payload);
 }
 
-function updateTodayDutyData(payload){
-    return axios.put('/api/v1/todayDuty',payload)
+function updateTodayDutyData(payload) {
+  return axios.put("/api/v1/todayDuty", payload);
 }
 
-function deleteTodayDutyData(payload){
-    return axios.delete('/api/v1/todayDuty',{params:payload})
+function deleteTodayDutyData(payload) {
+  return axios.delete("/api/v1/todayDuty", { params: payload });
 }
 
-export{
-    getTodayDutyData,
-    createTodayDutyData,
-    updateTodayDutyData,
-    deleteTodayDutyData
-}
+export {
+  getTodayDutyData,
+  createTodayDutyData,
+  updateTodayDutyData,
+  deleteTodayDutyData
+};
