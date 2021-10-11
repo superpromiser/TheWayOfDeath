1c002f4caed639242f02591ee368810a76dbd571
smilecoding7 <superpromiser@gmail.com>
2021-10-12 00:14:27 +0800

    updated

--- resources/js/api/sms.js
+++ resources/js/api/sms.js
@@ -1,24 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getSms(payload){
-    return axios.get('/api/v1/sms',{params:payload});
+function getSms(payload) {
+  return axios.get("/api/v1/sms", { params: payload });
 }
 
-function createSms(payload){
-    return axios.post('/api/v1/sms',payload)
+function createSms(payload) {
+  return axios.post("/api/v1/sms", payload);
 }
 
-function updateSms(payload){
-    return axios.put('/api/v1/sms',payload)
+function updateSms(payload) {
+  return axios.put("/api/v1/sms", payload);
 }
 
-function deleteSms(payload){
-    return axios.delete('/api/v1/sms',{data:payload})
+function deleteSms(payload) {
+  return axios.delete("/api/v1/sms", { data: payload });
 }
 
-export{
-    getSms,
-    createSms,
-    updateSms,
-    deleteSms
-}
+export { getSms, createSms, updateSms, deleteSms };
