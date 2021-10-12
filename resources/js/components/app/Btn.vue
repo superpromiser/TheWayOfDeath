92edb8cf0570d52824274d6ac732e310f334ca26
smilecoding7 <superpromiser@gmail.com>
2021-10-13 03:06:42 +0800

    updated

--- resources/js/components/app/Btn.vue
+++ resources/js/components/app/Btn.vue
@@ -1,22 +1,18 @@
 <template>
-  <v-btn
-    :color="color"
-    v-bind="$attrs"
-    v-on="$listeners"
-  >
+  <v-btn :color="color" v-bind="$attrs" v-on="$listeners">
     <slot />
   </v-btn>
 </template>
 
 <script>
-  export default {
-    name: 'AppBtn',
+export default {
+  name: "AppBtn",
 
-    props: {
-      color: {
-        type: String,
-        default: 'primary',
-      },
-    },
+  props: {
+    color: {
+      type: String,
+      default: "primary"
+    }
   }
+};
 </script>
