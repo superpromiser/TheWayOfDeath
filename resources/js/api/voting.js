8c281504b0df3f467d0e3f0b55690b7f18e169d6
smilecoding7 <superpromiser@gmail.com>
2021-10-13 03:06:06 +0800

    updated

--- resources/js/api/voting.js
+++ resources/js/api/voting.js
@@ -1,45 +1,44 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getVoting(payload){
-    return axios.get('/api/v1/voting',{params:payload})
+function getVoting(payload) {
+  return axios.get("/api/v1/voting", { params: payload });
 }
 
-function createVoting(payload){
-    return axios.post('/api/v1/voting',payload);
+function createVoting(payload) {
+  return axios.post("/api/v1/voting", payload);
 }
 
-function updateVoting(payload){
-    return axios.put('/api/v1/voting',payload);
+function updateVoting(payload) {
+  return axios.put("/api/v1/voting", payload);
 }
 
-function deleteVoting(payload){
-    return axios.delete('/api/v1/voting',{data:payload})
+function deleteVoting(payload) {
+  return axios.delete("/api/v1/voting", { data: payload });
 }
 
-function getTemplateList(payload){
-    return axios.get('/api/v1/voting/template',{params:payload});
+function getTemplateList(payload) {
+  return axios.get("/api/v1/voting/template", { params: payload });
 }
 
-function getTemplateCnt(payload){
-    return axios.get('/api/v1/voting/templateCnt',{params:payload});
+function getTemplateCnt(payload) {
+  return axios.get("/api/v1/voting/templateCnt", { params: payload });
 }
 
-function createTemplate(payload){
-    return axios.post('/api/v1/voting/template',payload)
+function createTemplate(payload) {
+  return axios.post("/api/v1/voting/template", payload);
 }
 
-function deleteTemplate(payload){
-    return axios.delete('/api/v1/voting/template',{data:payload});
+function deleteTemplate(payload) {
+  return axios.delete("/api/v1/voting/template", { data: payload });
 }
 
-
-export{
-    getVoting,
-    createVoting,
-    updateVoting,
-    deleteVoting,
-    getTemplateList,
-    createTemplate,
-    getTemplateCnt,
-    deleteTemplate
-}
+export {
+  getVoting,
+  createVoting,
+  updateVoting,
+  deleteVoting,
+  getTemplateList,
+  createTemplate,
+  getTemplateCnt,
+  deleteTemplate
+};
