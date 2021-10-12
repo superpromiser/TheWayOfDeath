45d6524cffcf2721a4d708f22279e779a42c36d3
smilecoding7 <superpromiser@gmail.com>
2021-10-13 03:05:41 +0800

    updated

--- resources/js/api/userRole.js
+++ resources/js/api/userRole.js
@@ -1,22 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
-function getUserRole(){
-    return axios.get('/api/v1/userRole');
+function getUserRole() {
+  return axios.get("/api/v1/userRole");
 }
-function createUserRole(payload){
-    return axios.post('/api/v1/userRole',payload)
+function createUserRole(payload) {
+  return axios.post("/api/v1/userRole", payload);
 }
-function updateUserRole(payload){
-    return axios.put('/api/v1/userRole',payload)
-}
-
-function deleteUserRole(payload){
-    return axios.delete('/api/v1/userRole',{data: payload})
+function updateUserRole(payload) {
+  return axios.put("/api/v1/userRole", payload);
 }
 
-export{
-    getUserRole,
-    createUserRole,
-    updateUserRole,
-    deleteUserRole,
+function deleteUserRole(payload) {
+  return axios.delete("/api/v1/userRole", { data: payload });
 }
+
+export { getUserRole, createUserRole, updateUserRole, deleteUserRole };
