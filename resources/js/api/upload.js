987c5c04087fdc4918fe450bce998899fb86bdcd
smilecoding7 <superpromiser@gmail.com>
2021-10-12 22:17:12 +0800

    updated

--- resources/js/api/upload.js
+++ resources/js/api/upload.js
@@ -1,32 +1,32 @@
-import axios from 'axios'
+import axios from "axios";
 
-function uploadImage(payload){
-    return axios.post('/api/v1/file/image', payload);
+function uploadImage(payload) {
+  return axios.post("/api/v1/file/image", payload);
 }
-function uploadVideo(payload){
-    return axios.post('/api/v1/file/video', payload);
+function uploadVideo(payload) {
+  return axios.post("/api/v1/file/video", payload);
 }
-function uploadOther(payload){
-    return axios.post('/api/v1/file/other', payload);
+function uploadOther(payload) {
+  return axios.post("/api/v1/file/other", payload);
 }
 
 function uploadOtherForExcel(payload) {
-    return axios.post('/api/v1/file/excel',payload);
+  return axios.post("/api/v1/file/excel", payload);
 }
-function deleteFile(payload){
-    return axios.delete('/api/v1/file?fileName=' + payload);
-    // return axios.delete( "/api/fileUpload/file?fileName=" + leagueImage, {headers: {"Authorization" : `Bearer ${token}`} },  )
-}
-
-function uploadExcelApi(payload){
-    return axios.post('/api/v1/file/jsonExcel',payload)
+function deleteFile(payload) {
+  return axios.delete("/api/v1/file?fileName=" + payload);
+  // return axios.delete( "/api/fileUpload/file?fileName=" + leagueImage, {headers: {"Authorization" : `Bearer ${token}`} },  )
 }
 
-export{
-    uploadImage,
-    uploadVideo,
-    uploadOther,
-    uploadOtherForExcel,
-    deleteFile,
-    uploadExcelApi,
+function uploadExcelApi(payload) {
+  return axios.post("/api/v1/file/jsonExcel", payload);
 }
+
+export {
+  uploadImage,
+  uploadVideo,
+  uploadOther,
+  uploadOtherForExcel,
+  deleteFile,
+  uploadExcelApi
+};
