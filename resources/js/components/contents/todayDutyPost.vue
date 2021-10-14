5bc570bf2e2aa2d03b76b350f843e241eb772da5
smilecoding7 <superpromiser@gmail.com>
2021-10-15 05:47:52 +0800

    updated

--- resources/js/components/contents/todayDutyPost.vue
+++ resources/js/components/contents/todayDutyPost.vue
@@ -1,22 +1,45 @@
 <template>
   <v-container v-if="$isMobile()">
     <v-row>
-      <v-col cols="12" class="d-flex hover-cursor-point" @click="showDetail(content)">
-        <v-avatar v-if="content.users.name !== '' && content.users.avatar == '/'" color="#7879ff" size="48">
-            <span class="white--text headline">{{content.users.name[0]}}</span>
-        </v-avatar>
-        <v-avatar v-else
+      <v-col
+        cols="12"
+        class="d-flex hover-cursor-point"
+        @click="showDetail(content)"
+      >
+        <v-avatar
+          v-if="content.users.name !== '' && content.users.avatar == '/'"
+          color="#7879ff"
           size="48"
         >
+          <span class="white--text headline">{{ content.users.name[0] }}</span>
+        </v-avatar>
+        <v-avatar v-else size="48">
           <v-img :src="content.users.avatar"></v-img>
         </v-avatar>
         <div class="ml-2 d-flex flex-column">
-          <p class="mb-0 font-size-0-95 font-weight-bold mb-auto primary-font-color"> {{lang.share}}  </p>
-          <p class="mb-0 font-size-0-8"><span class="font-color-gray">{{TimeViewMD(content.created_at)}}  </span> {{content.users.name}}</p>
+          <p
+            class="mb-0 font-size-0-95 font-weight-bold mb-auto primary-font-color"
+          >
+            {{ lang.share }}
+          </p>
+          <p class="mb-0 font-size-0-8">
+            <span class="font-color-gray"
+              >{{ TimeViewMD(content.created_at) }}
+            </span>
+            {{ content.users.name }}
+          </p>
         </div>
       </v-col>
       <v-col cols="12" class="pb-0">
-        <p class="text-wrap mb-0 font-size-0-75"><read-more more-str="全文" :text="shareData[0].text" link="#" less-str="收起" :max-chars="250"></read-more></p>
+        <p class="text-wrap mb-0 font-size-0-75">
+          <read-more
+            more-str="全文"
+            :text="shareData[0].text"
+            link="#"
+            less-str="收起"
+            :max-chars="250"
+          ></read-more>
+        </p>
       </v-col>
       <v-col cols="12" class="py-0" v-if="checkIfAttachExist(shareData[0])">
         <AttachItemViewer :items="shareData[0]" />
@@ -24,126 +47,160 @@
     </v-row>
   </v-container>
   <v-container v-else>
-    <v-col cols="12" class="d-flex align-center hover-cursor-point" @click="showDetail(content)">
-      <v-avatar v-if="content.users.name !== '' && content.users.avatar == '/'" color="#7879ff" size="60" class="ma-5">
-          <span class="white--text headline">{{content.users.name[0]}}</span>
-      </v-avatar>
-      <v-avatar v-else
-        class="ma-5"
+    <v-col
+      cols="12"
+      class="d-flex align-center hover-cursor-point"
+      @click="showDetail(content)"
+    >
+      <v-avatar
+        v-if="content.users.name !== '' && content.users.avatar == '/'"
+        color="#7879ff"
         size="60"
+        class="ma-5"
       >
+        <span class="white--text headline">{{ content.users.name[0] }}</span>
+      </v-avatar>
+      <v-avatar v-else class="ma-5" size="60">
         <v-img :src="content.users.avatar"></v-img>
       </v-avatar>
       <div>
-        <p class="font-weight-black fs-15 mb-3"> {{lang.todayDuty}}</p>
+        <p class="font-weight-black fs-15 mb-3">{{ lang.todayDuty }}</p>
         <div class="d-flex align-center">
-          <v-icon medium color="#7879ff" class="mr-2">mdi-clock-outline </v-icon>
-          <p class="mb-0 mr-8">{{TimeView(content.created_at)}}</p>
+          <v-icon medium color="#7879ff" class="mr-2"
+            >mdi-clock-outline
+          </v-icon>
+          <p class="mb-0 mr-8">{{ TimeView(content.created_at) }}</p>
           <v-icon medium color="#7879ff" class="mr-2">mdi-account </v-icon>
-          <p class="mb-0">{{content.users.name}}</p>
+          <p class="mb-0">{{ content.users.name }}</p>
         </div>
       </div>
-      <div class="ml-auto" v-if="user.roleId < 3 || content.users.id == user.id">
-        <v-menu offset-y >
+      <div
+        class="ml-auto"
+        v-if="user.roleId < 3 || content.users.id == user.id"
+      >
+        <v-menu offset-y>
           <template v-slot:activator="{ attrs, on }">
-            <v-btn icon color="#7879ff" v-bind="attrs" v-on="on" >
+            <v-btn icon color="#7879ff" v-bind="attrs" v-on="on">
               <v-icon size="30">mdi-chevron-down </v-icon>
             </v-btn>
           </template>
           <v-list>
-            <v-list-item link v-if="user.roleId < 3" >
-              <v-list-item-title class="px-2" @click="fixTop(content)" v-if="content.fixTop == null">{{lang.toTop}}</v-list-item-title>
-              <v-list-item-title class="px-2" @click="relaseTop(content.id)" v-else>{{lang.toRelase}}</v-list-item-title>
+            <v-list-item link v-if="user.roleId < 3">
+              <v-list-item-title
+                class="px-2"
+                @click="fixTop(content)"
+                v-if="content.fixTop == null"
+                >{{ lang.toTop }}</v-list-item-title
+              >
+              <v-list-item-title
+                class="px-2"
+                @click="relaseTop(content.id)"
+                v-else
+                >{{ lang.toRelase }}</v-list-item-title
+              >
             </v-list-item>
-            <v-list-item link >
-              <v-list-item-title class="px-2" @click="postRemove(content)">{{lang.remove}}</v-list-item-title>
+            <v-list-item link>
+              <v-list-item-title class="px-2" @click="postRemove(content)">{{
+                lang.remove
+              }}</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
       </div>
     </v-col>
     <v-col cols="12" class="pl-10 pt-0">
-      日期:{{content.todayduty.dutyDate}}
+      日期:{{ content.todayduty.dutyDate }}
     </v-col>
     <v-row class="px-10 ma-0">
-      <v-col cols="3" v-for="dutyUser in content.todayduty.userList" :key="dutyUser.id">
-        <v-avatar v-if="dutyUser.name !== '' && dutyUser.avatar == '/'" color="#7879ff" size="25">
-            <span class="white--text headline">{{dutyUser.name[0]}}</span>
-        </v-avatar>
-        <v-avatar v-else
+      <v-col
+        cols="3"
+        v-for="dutyUser in content.todayduty.userList"
+        :key="dutyUser.id"
+      >
+        <v-avatar
+          v-if="dutyUser.name !== '' && dutyUser.avatar == '/'"
+          color="#7879ff"
           size="25"
         >
+          <span class="white--text headline">{{ dutyUser.name[0] }}</span>
+        </v-avatar>
+        <v-avatar v-else size="25">
           <v-img :src="dutyUser.avatar"></v-img>
         </v-avatar>
-        <span>{{dutyUser.name}}</span>
+        <span>{{ dutyUser.name }}</span>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import lang from '~/helper/lang.json';
-import AttachItemViewer from '~/components/attachItemViewer';
-import {createReadCnt} from '~/api/alarm'
-import {mapGetters} from 'vuex'
+import lang from "~/helper/lang.json";
+import AttachItemViewer from "~/components/attachItemViewer";
+import { createReadCnt } from "~/api/alarm";
+import { mapGetters } from "vuex";
 export default {
-    components:{
-        AttachItemViewer,
-    },
+  components: {
+    AttachItemViewer
+  },
 
-    props:{
-        content:{
-            type:Object,
-            required:true
-        }
-    },
-    data:() => ({
-        lang,
-        baseUrl:window.Laravel.base_url,
-        shareData: {},
-    }),
-    created(){
-      console.log('todayduty post-----',this.content.todayduty.userList)
-      // this.shareData = JSON.parse(this.content.shares.content);
-    },
-    computed:{
-      currentPath(){
-        return this.$route
-      },
-      ...mapGetters({
-        user:'auth/user',
-        selectedSchoolItem : 'mo/selectedSchoolItem',
-      })
-    },
-    methods:{
-
-      showDetail(content){
-        createReadCnt({postId:content.id}).then(res=>{
-        }).catch(err=>{
-          console.log(err.response)
-        })
-        if(this.$isMobile()){
-          if(this.selectedSchoolItem.type == 'school'){
-            this.$router.push({name:'details.schoolDefault', params:{schoolId: this.selectedSchoolItem.schoolId}});
-          }
-          else{
-            this.$router.push({name:'details.classDefault', params:{schoolId: this.selectedSchoolItem.schoolId,gradeId: this.selectedSchoolItem.gradeId, lessonId: this.selectedSchoolItem.lessonId}});
-          }
-        }else{
-          this.$store.dispatch('content/storePostDetail',content)
-          if(this.currentPath.params.lessonId){
-            this.$router.push({name:'details.classDefault'});
-          }else{
-            this.$router.push({name:'details.schoolDefault'});
-          }
-        }
-      },
-      
+  props: {
+    content: {
+      type: Object,
+      required: true
     }
-
-}
+  },
+  data: () => ({
+    lang,
+    baseUrl: window.Laravel.base_url,
+    shareData: {}
+  }),
+  created() {
+    console.log("todayduty post-----", this.content.todayduty.userList);
+    // this.shareData = JSON.parse(this.content.shares.content);
+  },
+  computed: {
+    currentPath() {
+      return this.$route;
+    },
+    ...mapGetters({
+      user: "auth/user",
+      selectedSchoolItem: "mo/selectedSchoolItem"
+    })
+  },
+  methods: {
+    showDetail(content) {
+      createReadCnt({ postId: content.id })
+        .then(res => {})
+        .catch(err => {
+          console.log(err.response);
+        });
+      if (this.$isMobile()) {
+        if (this.selectedSchoolItem.type == "school") {
+          this.$router.push({
+            name: "details.schoolDefault",
+            params: { schoolId: this.selectedSchoolItem.schoolId }
+          });
+        } else {
+          this.$router.push({
+            name: "details.classDefault",
+            params: {
+              schoolId: this.selectedSchoolItem.schoolId,
+              gradeId: this.selectedSchoolItem.gradeId,
+              lessonId: this.selectedSchoolItem.lessonId
+            }
+          });
+        }
+      } else {
+        this.$store.dispatch("content/storePostDetail", content);
+        if (this.currentPath.params.lessonId) {
+          this.$router.push({ name: "details.classDefault" });
+        } else {
+          this.$router.push({ name: "details.schoolDefault" });
+        }
+      }
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
