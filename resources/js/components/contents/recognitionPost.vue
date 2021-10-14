5ec312e541f4b224e0f55febb8465eaa030438e6
smilecoding7 <superpromiser@gmail.com>
2021-10-15 02:26:20 +0800

    updated

--- resources/js/components/contents/recognitionPost.vue
+++ resources/js/components/contents/recognitionPost.vue
@@ -2,127 +2,178 @@
   <v-container v-if="$isMobile()">
     <v-row>
       <v-col cols="12" class="d-flex" @click="showDetail(content)">
-        <v-avatar v-if="content.users.name !== '' && content.users.avatar == '/'" color="#7879ff" size="48">
-          <span class="white--text headline">{{content.users.name[0]}}</span>
-        </v-avatar>
-        <v-avatar v-else
+        <v-avatar
+          v-if="content.users.name !== '' && content.users.avatar == '/'"
+          color="#7879ff"
           size="48"
         >
+          <span class="white--text headline">{{ content.users.name[0] }}</span>
+        </v-avatar>
+        <v-avatar v-else size="48">
           <v-img :src="content.users.avatar"></v-img>
         </v-avatar>
         <div class="ml-2 d-flex flex-column">
-          <p class="mb-0 font-size-0-95 font-weight-bold mb-auto primary-font-color"> {{lang.recognition}}  </p>
-          <p class="mb-0 font-size-0-8"><span class="font-color-gray">{{TimeViewMD(content.created_at)}}  </span> {{content.users.name}}</p>
+          <p
+            class="mb-0 font-size-0-95 font-weight-bold mb-auto primary-font-color"
+          >
+            {{ lang.recognition }}
+          </p>
+          <p class="mb-0 font-size-0-8">
+            <span class="font-color-gray"
+              >{{ TimeViewMD(content.created_at) }}
+            </span>
+            {{ content.users.name }}
+          </p>
         </div>
       </v-col>
       <v-col cols="12" class="">
-        <v-chip small v-for="student in students" :key="student.id" class="ml-2 hover-cursor-point">
-          @{{student.name}}
+        <v-chip
+          small
+          v-for="student in students"
+          :key="student.id"
+          class="ml-2 hover-cursor-point"
+        >
+          @{{ student.name }}
         </v-chip>
       </v-col>
     </v-row>
   </v-container>
   <v-container v-else>
-      <v-col cols="12" class="d-flex align-center hover-cursor-point" @click="showDetail(content)">
-          <v-avatar v-if="content.users.name !== '' && content.users.avatar == '/'" color="#7879ff" size="60" class="ma-5">
-              <span class="white--text headline">{{content.users.name[0]}}</span>
-          </v-avatar>
-          <v-avatar v-else
-            class="ma-5"
-            size="60"
-          >
-            <v-img :src="content.users.avatar"></v-img>
-          </v-avatar>
-          <div>
-            <p class="font-weight-black fs-15 mb-3"> {{lang.recognition}}  </p>
-            <div class="d-flex align-center">
-              <v-icon medium color="#7879ff" class="mr-2">mdi-clock-outline </v-icon>
-              <p class="mb-0 mr-8">{{TimeView(content.created_at)}}</p>
-              <v-icon medium color="#7879ff" class="mr-2">mdi-account </v-icon>
-              <p class="mb-0">{{content.users.name}}</p>
-            </div>
-          </div>
-          <div class="ml-auto" v-if="user.roleId < 3 || content.users.id == user.id">
-            <v-menu offset-y >
-              <template v-slot:activator="{ attrs, on }">
-                <v-btn icon color="#7879ff" v-bind="attrs" v-on="on" >
-                  <v-icon size="30">mdi-chevron-down </v-icon>
-                </v-btn>
-              </template>
-              <v-list>
-                <v-list-item link v-if="user.roleId < 3" >
-                  <v-list-item-title class="px-2" @click="fixTop(content)" v-if="content.fixTop == null">{{lang.toTop}}</v-list-item-title>
-                  <v-list-item-title class="px-2" @click="relaseTop(content.id)" v-else>{{lang.toRelase}}</v-list-item-title>
-                </v-list-item>
-                <v-list-item link >
-                  <v-list-item-title class="px-2" @click="postRemove(content)">{{lang.remove}}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-          </div>
-      </v-col>
-      <v-col cols="12" class="pl-10 pt-0">
-        <v-chip v-for="student in students" :key="student.id" class="ml-2 hover-cursor-point">
-          @{{student.name}}
-        </v-chip>
-      </v-col>
+    <v-col
+      cols="12"
+      class="d-flex align-center hover-cursor-point"
+      @click="showDetail(content)"
+    >
+      <v-avatar
+        v-if="content.users.name !== '' && content.users.avatar == '/'"
+        color="#7879ff"
+        size="60"
+        class="ma-5"
+      >
+        <span class="white--text headline">{{ content.users.name[0] }}</span>
+      </v-avatar>
+      <v-avatar v-else class="ma-5" size="60">
+        <v-img :src="content.users.avatar"></v-img>
+      </v-avatar>
+      <div>
+        <p class="font-weight-black fs-15 mb-3">{{ lang.recognition }}</p>
+        <div class="d-flex align-center">
+          <v-icon medium color="#7879ff" class="mr-2"
+            >mdi-clock-outline
+          </v-icon>
+          <p class="mb-0 mr-8">{{ TimeView(content.created_at) }}</p>
+          <v-icon medium color="#7879ff" class="mr-2">mdi-account </v-icon>
+          <p class="mb-0">{{ content.users.name }}</p>
+        </div>
+      </div>
+      <div
+        class="ml-auto"
+        v-if="user.roleId < 3 || content.users.id == user.id"
+      >
+        <v-menu offset-y>
+          <template v-slot:activator="{ attrs, on }">
+            <v-btn icon color="#7879ff" v-bind="attrs" v-on="on">
+              <v-icon size="30">mdi-chevron-down </v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item link v-if="user.roleId < 3">
+              <v-list-item-title
+                class="px-2"
+                @click="fixTop(content)"
+                v-if="content.fixTop == null"
+                >{{ lang.toTop }}</v-list-item-title
+              >
+              <v-list-item-title
+                class="px-2"
+                @click="relaseTop(content.id)"
+                v-else
+                >{{ lang.toRelase }}</v-list-item-title
+              >
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-title class="px-2" @click="postRemove(content)">{{
+                lang.remove
+              }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+    </v-col>
+    <v-col cols="12" class="pl-10 pt-0">
+      <v-chip
+        v-for="student in students"
+        :key="student.id"
+        class="ml-2 hover-cursor-point"
+      >
+        @{{ student.name }}
+      </v-chip>
+    </v-col>
   </v-container>
 </template>
 
 <script>
-import lang from '~/helper/lang.json'
-import {createReadCnt} from '~/api/alarm'
-import {mapGetters} from 'vuex'
+import lang from "~/helper/lang.json";
+import { createReadCnt } from "~/api/alarm";
+import { mapGetters } from "vuex";
 export default {
-    props:{
-        content:{
-            type:Object,
-            required:true
+  props: {
+    content: {
+      type: Object,
+      required: true
+    }
+  },
+  data: () => ({
+    lang,
+    baseUrl: window.Laravel.base_url,
+    students: []
+  }),
+  computed: {
+    currentPath() {
+      return this.$route;
+    },
+    ...mapGetters({
+      user: "auth/user",
+      selectedSchoolItem: "mo/selectedSchoolItem"
+    })
+  },
+  methods: {
+    showDetail(content) {
+      createReadCnt({ postId: content.id })
+        .then(res => {})
+        .catch(err => {
+          console.log(err.response);
+        });
+      this.$store.dispatch("content/storePostDetail", content);
+      if (this.$isMobile()) {
+        if (this.selectedSchoolItem.type == "school") {
+          this.$router.push({
+            name: "details.classRecognition",
+            params: { schoolId: this.selectedSchoolItem.schoolId }
+          });
+        } else {
+          this.$router.push({
+            name: "details.classRecognition",
+            params: {
+              schoolId: this.selectedSchoolItem.schoolId,
+              gradeId: this.selectedSchoolItem.gradeId,
+              lessonId: this.selectedSchoolItem.lessonId
+            }
+          });
         }
-    },
-    data:()=>({
-        lang,
-        baseUrl:window.Laravel.base_url,
-        students:[],
-    }),
-    computed:{
-      currentPath(){
-        return this.$route
-      },
-      ...mapGetters({
-        user:'auth/user',
-        selectedSchoolItem : 'mo/selectedSchoolItem',
-      })
-    },
-    methods:{
-      showDetail(content){
-        createReadCnt({postId:content.id}).then(res=>{
-        }).catch(err=>{
-          console.log(err.response)
-        })
-        this.$store.dispatch('content/storePostDetail',content)
-        if(this.$isMobile()){
-          if(this.selectedSchoolItem.type == 'school'){
-            this.$router.push({name:'details.classRecognition', params:{schoolId: this.selectedSchoolItem.schoolId}});
-          }
-          else{
-            this.$router.push({name:'details.classRecognition', params:{schoolId: this.selectedSchoolItem.schoolId,gradeId: this.selectedSchoolItem.gradeId, lessonId: this.selectedSchoolItem.lessonId}});
-          }
-        }else{
-          if(this.currentPath.params.lessonId){
-            this.$router.push({name:'details.classRecognition'});
-          }else{
-            this.$router.push({name:'details.classRecognition'});
-          }
+      } else {
+        if (this.currentPath.params.lessonId) {
+          this.$router.push({ name: "details.classRecognition" });
+        } else {
+          this.$router.push({ name: "details.classRecognition" });
         }
       }
-    },
-    created(){
-      this.students = JSON.parse(this.content.recognitions.students)
-    },
-}
+    }
+  },
+  created() {
+    this.students = JSON.parse(this.content.recognitions.students);
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
