6e49e108c82315886721947850aa5c9d6e091094
smilecoding7 <superpromiser@gmail.com>
2021-10-18 04:42:29 +0800

    updated

--- resources/js/components/Snackbar.vue
+++ resources/js/components/Snackbar.vue
@@ -3,53 +3,55 @@
     {{ message }}
     <v-btn text @click="show = false">Close</v-btn>
   </v-snackbar> -->
-  <v-snackbar v-if="$isMobile()"
-        timeout="3000"
-        v-model="show"
-        color="#999999"
-        rounded="pill"
-        absolute
-        dark
-        top
-        class="z-index-10 position-fixed mo-snackbar"
-        >
-        {{ message }}
-    </v-snackbar>
   <v-snackbar
-        v-else
-        timeout="3000"
-        v-model="show"
-        :color="color"
-        absolute
-        top
-        class="z-index-10 position-fixed"
-        >
-        {{ message }}
-        <template v-slot:action="{ attrs }">
-            <v-btn v-bind="attrs" icon dark @click="show = false"> <v-icon>mdi-close</v-icon> </v-btn>
-        </template>
-        
-    </v-snackbar>
+    v-if="$isMobile()"
+    timeout="3000"
+    v-model="show"
+    color="#999999"
+    rounded="pill"
+    absolute
+    dark
+    top
+    class="z-index-10 position-fixed mo-snackbar"
+  >
+    {{ message }}
+  </v-snackbar>
+  <v-snackbar
+    v-else
+    timeout="3000"
+    v-model="show"
+    :color="color"
+    absolute
+    top
+    class="z-index-10 position-fixed"
+  >
+    {{ message }}
+    <template v-slot:action="{ attrs }">
+      <v-btn v-bind="attrs" icon dark @click="show = false">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </template>
+  </v-snackbar>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       show: false,
-      message: '',
-      color: ''
-    }
+      message: "",
+      color: ""
+    };
   },
 
-  created () {
+  created() {
     this.$store.subscribe((mutation, state) => {
-      if (mutation.type === 'snackbar/showMessage') {
-        this.message = state.snackbar.content
-        this.color = state.snackbar.color
-        this.show = true
+      if (mutation.type === "snackbar/showMessage") {
+        this.message = state.snackbar.content;
+        this.color = state.snackbar.color;
+        this.show = true;
       }
-    })
+    });
   }
-}
+};
 </script>
