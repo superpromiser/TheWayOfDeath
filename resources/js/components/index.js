f8bba3cf863b904bb81d29992f4bafba94190a9f
smilecoding7 <superpromiser@gmail.com>
2021-10-22 16:04:29 +0800

    updated

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,19 +1,13 @@
-import Vue from 'vue'
-import Card from './Card'
-import Child from './Child'
-import Button from './Button'
-import Checkbox from './Checkbox'
-import { HasError, AlertError, AlertSuccess } from 'vform'
+import Vue from "vue";
+import Card from "./Card";
+import Child from "./Child";
+import Button from "./Button";
+import Checkbox from "./Checkbox";
+import { HasError, AlertError, AlertSuccess } from "vform";
 
 // Components that are registered globaly.
-[
-  Card,
-  Child,
-  Button,
-  Checkbox,
-  HasError,
-  AlertError,
-  AlertSuccess
-].forEach(Component => {
-  Vue.component(Component.name, Component)
-})
+[Card, Child, Button, Checkbox, HasError, AlertError, AlertSuccess].forEach(
+  Component => {
+    Vue.component(Component.name, Component);
+  }
+);
