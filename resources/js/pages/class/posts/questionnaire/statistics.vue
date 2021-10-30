a4101d8dc8554a8fd93efac245735fa1d2066a72
ericli0702 <smiledream0702@gmail.com>
2021-10-31 03:28:34 +0800

    updated

--- resources/js/pages/class/posts/questionnaire/statistics.vue
+++ resources/js/pages/class/posts/questionnaire/statistics.vue
@@ -1,119 +1,127 @@
 <template>
   <v-container>
-      <div class="mt-3 mb-10">
-          <QuestionItem class="mt-10" :Label="lang.contentPlace" ref="child" @contentData="loadContentData"/>
-          <v-divider></v-divider>
-      </div>
-      <v-row>
-        <v-col cols="12" md="6">
-          <v-row>
-            <v-col cols="12">{{lang.numberRange}}</v-col>
-            <v-col cols="12" class="d-flex align-end justify-content-around">
-              <v-text-field
-                :label="lang.startingValue"
-                v-model="statData.statDataArr[0].sValue"
-                hide-details
-              ></v-text-field>
-              <p class="mb-0 mx-5">{{lang.to}}</p>
-              <v-text-field
-                :label="lang.endValue"
-                v-model="statData.statDataArr[0].eValue"
-                hide-details
-              ></v-text-field>
-            </v-col>
-          </v-row>
-        </v-col>
-        <v-col class="12" md="6">
-          <v-row>
-            <v-col cols="12">{{lang.unit}}</v-col>
-            <v-col cols="12">
-              <v-text-field
-                :label="lang.endValue"
-                v-model="statData.statDataArr[0].unit"
-                hide-details
-              ></v-text-field>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-      <v-container>
-        <v-row class="my-10 d-flex align-center">
-            <v-btn large rounded dark color="#7879ff" @click="addStatContent">{{lang.submit}}</v-btn>
+    <div class="mt-3 mb-10">
+      <QuestionItem
+        class="mt-10"
+        :Label="lang.contentPlace"
+        ref="child"
+        @contentData="loadContentData"
+      />
+      <v-divider></v-divider>
+    </div>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-row>
+          <v-col cols="12">{{ lang.numberRange }}</v-col>
+          <v-col cols="12" class="d-flex align-end justify-content-around">
+            <v-text-field
+              :label="lang.startingValue"
+              v-model="statData.statDataArr[0].sValue"
+              hide-details
+            ></v-text-field>
+            <p class="mb-0 mx-5">{{ lang.to }}</p>
+            <v-text-field
+              :label="lang.endValue"
+              v-model="statData.statDataArr[0].eValue"
+              hide-details
+            ></v-text-field>
+          </v-col>
         </v-row>
-      </v-container>
-      <v-snackbar
-        timeout="3000"
-        v-model="requiredText"
-        color="error"
-        absolute
-        top
-        >
-        {{lang.requiredText}}
-      </v-snackbar>
+      </v-col>
+      <v-col class="12" md="6">
+        <v-row>
+          <v-col cols="12">{{ lang.unit }}</v-col>
+          <v-col cols="12">
+            <v-text-field
+              :label="lang.endValue"
+              v-model="statData.statDataArr[0].unit"
+              hide-details
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-container>
+      <v-row class="my-10 d-flex align-center">
+        <v-btn large rounded dark color="#7879ff" @click="addStatContent">{{
+          lang.submit
+        }}</v-btn>
+      </v-row>
+    </v-container>
+    <v-snackbar
+      timeout="3000"
+      v-model="requiredText"
+      color="error"
+      absolute
+      top
+    >
+      {{ lang.requiredText }}
+    </v-snackbar>
   </v-container>
 </template>
 
 <script>
-import QuestionItem from '~/components/questionItem';
-import lang from '~/helper/lang.json';
+import QuestionItem from "~/components/questionItem";
+import lang from "~/helper/lang.json";
 
 export default {
-  components:{
-    QuestionItem,
+  components: {
+    QuestionItem
   },
-  props:{
-      type:{
-          type:String,
-          required:false
-      }
+  props: {
+    type: {
+      type: String,
+      required: false
+    }
   },
   data: () => ({
-    statData : {
-      statDataArr:[
+    statData: {
+      statDataArr: [
         {
-          contentData:[
-          ],
-          sValue:'',
-          eValue:'',
-          unit:''
+          contentData: [],
+          sValue: "",
+          eValue: "",
+          unit: ""
         }
       ],
-      type : 'stat'
+      type: "stat"
     },
     lang,
-    requiredText:false,
+    requiredText: false
   }),
 
-  methods:{
-    addStatContent(){
-      this.$refs.child.emitData()
+  methods: {
+    addStatContent() {
+      this.$refs.child.emitData();
       //console.log(this.statData.statDataArr[0])
-      if(this.statData.statDataArr[0].contentData.length == 0 || this.statData.statDataArr[0].sValue == '' || this.statData.statDataArr[0].eValue == '' || this.statData.statDataArr[0].unit == ''){
+      if (
+        this.statData.statDataArr[0].contentData.length == 0 ||
+        this.statData.statDataArr[0].sValue == "" ||
+        this.statData.statDataArr[0].eValue == "" ||
+        this.statData.statDataArr[0].unit == ""
+      ) {
         this.requiredText = true;
         this.statData.statDataArr[0].contentData = [];
         return;
       }
-      
-      this.$emit('contentData',this.statData);
-      if(this.type == 'post'){
-          this.$router.push({name:'posts.Cquestionnaire'});
-      }else{
-          this.$router.push({name:'Cquestionnaire.templateNew'})
+
+      this.$emit("contentData", this.statData);
+      if (this.type == "post") {
+        this.$router.push({ name: "posts.Cquestionnaire" });
+      } else {
+        this.$router.push({ name: "Cquestionnaire.templateNew" });
       }
     },
-    loadContentData(data){
+    loadContentData(data) {
       //console.log(data)
-      if(data.text === ''){
-        this.statData.statDataArr[0].contentData = []
+      if (data.text === "") {
+        this.statData.statDataArr[0].contentData = [];
         return;
       }
       this.statData.statDataArr[0].contentData.push(data);
     }
   }
-
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
