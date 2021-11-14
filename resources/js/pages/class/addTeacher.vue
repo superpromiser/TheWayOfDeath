054cae629ac3489b091d13f9d8e4b2c945ac14cf
ericli0702 <smiledream0702@gmail.com>
2021-11-14 13:53:43 +0800

    updated

--- resources/js/pages/class/addTeacher.vue
+++ resources/js/pages/class/addTeacher.vue
@@ -1,33 +1,30 @@
 <template>
-    <AddTeacherMember @onSelectMember="onSelectMember"/>
+  <AddTeacherMember @onSelectMember="onSelectMember" />
 </template>
 
 <script>
-import AddTeacherMember from '~/components/addTeacherMember'
-import {mapGetters} from 'vuex';
+import AddTeacherMember from "~/components/addTeacherMember";
+import { mapGetters } from "vuex";
 export default {
-    components:{
-        AddTeacherMember,
-    },
-    computed:{
-        ...mapGetters({
-            isDetail: 'returnteam/isDetail'
-        })
-    },
-    methods:{
-        onSelectMember(selectedTeacher){
-            this.$store.dispatch('member/storeSelectedTeacher', selectedTeacher);
-            if(this.isDetail == true){
-                this.$router.push({name: 'classSpace.detailReturnTeam'});
-            }
-            else{
-                this.$router.push({name: 'classSpace.newReturnTeam'});
-            }
-        },
+  components: {
+    AddTeacherMember
+  },
+  computed: {
+    ...mapGetters({
+      isDetail: "returnteam/isDetail"
+    })
+  },
+  methods: {
+    onSelectMember(selectedTeacher) {
+      this.$store.dispatch("member/storeSelectedTeacher", selectedTeacher);
+      if (this.isDetail == true) {
+        this.$router.push({ name: "classSpace.detailReturnTeam" });
+      } else {
+        this.$router.push({ name: "classSpace.newReturnTeam" });
+      }
     }
-}
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
